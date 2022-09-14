647913b712d71bbc2393e4aef896948301b47b58
Mouhamed DIOP <mouhamed.diop89@gmail.com>
2022-09-14 13:14:16 +0000

    Translated using Weblate (French)
    Currently translated at 35.9% (953 of 2652 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/fr/

--- translated/master/Slicer_fr-FR.ts
+++ translated/master/Slicer_fr-FR.ts
@@ -151,7 +151,7 @@
     <message>
         <location filename="Modules/Loadable/Markups/qSlicerMarkupsModule.cxx" line="106"/>
         <source>Markups</source>
-        <translation>Balisages</translation>
+        <translation type="unfinished">Balisages</translation>
     </message>
     <message>
         <location filename="Modules/Loadable/Sequences/qSlicerSequencesModule.cxx" line="79"/>
