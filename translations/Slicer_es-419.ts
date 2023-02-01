f6125d6306cef3c949da2e1c98d46b0f9b5b46b5
VianMJ <vmunozj@uaemex.mx>
2023-02-01 23:49:55 +0000

    Translated using Weblate (Spanish (Latin America))
    Currently translated at 2.9% (125 of 4179 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/es_419/

--- translations/Slicer_es-419.ts
+++ translations/Slicer_es-419.ts
@@ -21741,7 +21741,7 @@ This should only used by advanced users who understand the consequences of chang
     <message>
         <location filename="Modules/Loadable/VolumeRendering/Resources/UI/qSlicerVolumeRenderingModuleWidget.ui" line="172"/>
         <source>Display</source>
-        <translation type="unfinished">Display</translation>
+        <translation>Desplegar</translation>
     </message>
     <message>
         <location filename="Modules/Loadable/VolumeRendering/Resources/UI/qSlicerVolumeRenderingModuleWidget.ui" line="199"/>
