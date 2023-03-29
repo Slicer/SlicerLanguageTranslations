a675f0fa992444cb4eeb133b972b6e0517f00835
Idrissa Seck <idrisecksa@gmail.com>
2023-03-29 12:25:47 +0000

    Translated using Weblate (French)
    Currently translated at 64.3% (2728 of 4238 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/fr/

--- translations/Slicer_fr-FR.ts
+++ translations/Slicer_fr-FR.ts
@@ -22149,12 +22149,12 @@ Cette option ne doit être utilisée que par des utilisateurs avancés qui compr
     <message>
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="238"/>
         <source>Fine lines</source>
-        <translation type="unfinished"></translation>
+        <translation>Lignes fines</translation>
     </message>
     <message>
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="239"/>
         <source>Fine lines.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lignes fines</translation>
     </message>
     <message>
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="244"/>
