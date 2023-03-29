49aa697c19cf64de3d2685104babbb211aa7e09d
Idrissa Seck <idrisecksa@gmail.com>
2023-03-29 12:28:07 +0000

    Translated using Weblate (French)
    Currently translated at 64.4% (2733 of 4238 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/fr/

--- translations/Slicer_fr-FR.ts
+++ translations/Slicer_fr-FR.ts
@@ -22182,7 +22182,7 @@ Cette option ne doit être utilisée que par des utilisateurs avancés qui compr
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="313"/>
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="320"/>
         <source>Slice intersections</source>
-        <translation type="unfinished">Slice intersections</translation>
+        <translation>Intersections de tranche</translation>
     </message>
     <message>
         <location filename="Base/QTGUI/qSlicerViewersToolBar.cxx" line="321"/>
