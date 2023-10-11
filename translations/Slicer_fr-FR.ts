19c1d527cc112cc9d67b424a5ca92eea3e0c3f88
FatouBintou22 <fbndiaye99@gmail.com>
2023-10-11 12:59:35 +0000

    Translated using Weblate (French)
    Currently translated at 98.8% (5217 of 5278 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/fr/

--- translations/Slicer_fr-FR.ts
+++ translations/Slicer_fr-FR.ts
@@ -10627,7 +10627,11 @@ Si les segments se chevauchent, le segment situé plus haut dans le tableau des 
 &lt;li&gt;&lt;b&gt;Use for masking:&lt;/b&gt; set the selected intensity range as &lt;dfn&gt;Editable intensity range&lt;/dfn&gt; and switch to Paint effect.
 &lt;li&gt;&lt;b&gt;Apply:&lt;/b&gt; set the previewed segmentation in the selected segment. Previous contents of the segment is overwritten.
 &lt;/ul&gt;&lt;p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Remplissez le segment en fonction de la plage d&apos;intensité du volume source&lt;br&gt;. Options&#xa0;:&lt;p&gt;
+&lt;ul style=&quot;marge&#xa0;: 0&quot;&gt;
+&lt;li&gt;&lt;b&gt;Utiliser pour le masquage&#xa0;:&lt;/b&gt; définissez la plage d&apos;intensité sélectionnée sur &lt;dfn&gt;Plage d&apos;intensité modifiable&lt;/dfn&gt; et passez à l&apos;effet Peinture.
+&lt;li&gt;&lt;b&gt;Appliquer&#xa0;:&lt;/b&gt; définit la segmentation prévisualisée dans le segment sélectionné. Le contenu précédent du segment est écrasé.
+&lt;/ul&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="Modules/Loadable/Segmentations/EditorEffects/Python/SegmentEditorEffects/SegmentEditorThresholdEffect.py" line="151"/>
