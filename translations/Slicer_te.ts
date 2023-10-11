0d43b98cc089035e74f56c0528b710e55d218a36
Siddhardha Manduru <siddhardha.manduru@gmail.com>
2023-10-11 13:03:54 +0000

    Translated using Weblate (Telugu)
    Currently translated at 0.2% (12 of 5278 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/te/

--- translations/Slicer_te.ts
+++ translations/Slicer_te.ts
@@ -76,7 +76,7 @@
     <message>
         <location filename="Modules/CLI/AddScalarVolumes/AddScalarVolumes_tr.h" line="18"/>
         <source>Interpolation order</source>
-        <translation>ఇంటర్‌పోలేషన్ ఆర్డర్</translation>
+        <translation type="unfinished">ఇంటర్‌పోలేషన్ ఆర్డర్</translation>
     </message>
 </context>
 <context>
