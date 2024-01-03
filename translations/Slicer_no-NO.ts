11172ad0072ffc63aec94852d620be66958731a3
Allan Nordhøy <epost@anotheragency.no>
2024-01-03 18:43:23 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 2.1% (114 of 5382 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/nb_NO/

--- translations/Slicer_no-NO.ts
+++ translations/Slicer_no-NO.ts
@@ -18041,7 +18041,7 @@ p, li { white-space: pre-wrap; }
         <location filename="Docs/_extracli/BRAINSTransformConvert_tr.h" line="3"/>
         <location filename="Modules/CLI/ResampleDTIVolume/ResampleDTIVolume_tr.h" line="4"/>
         <source>Utilities</source>
-        <translation type="unfinished"></translation>
+        <translation>Verktøy</translation>
     </message>
     <message>
         <location filename="Docs/_extracli/BRAINSDeface_tr.h" line="4"/>
