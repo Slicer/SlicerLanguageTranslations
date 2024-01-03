cd462c008561ca4fea1b347a4c7a63285b74670f
Andras Lasso <lasso@queensu.ca>
2024-01-03 15:28:52 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 94.9% (56 of 59 strings)
    
    Translation: 3D Slicer/LanguagePacks
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/languagepacks/hu/

--- translations/LanguagePacks_hu.ts
+++ translations/LanguagePacks_hu.ts
@@ -6,67 +6,67 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="30"/>
         <source>Download, compile, and install latest translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Töltse le, kompile és telepítse a legújabb fordításokat</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="33"/>
         <source>Update translation files</source>
-        <translation type="unfinished"></translation>
+        <translation>Frissítse a fordítási fájlokat</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="47"/>
         <source>Input translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Fordítás források</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="56"/>
         <source>Input folder:</source>
-        <translation type="unfinished"></translation>
+        <translation>Forrás mappa:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="66"/>
         <source>Languages:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyelvek:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="76"/>
         <source>Weblate (latest translations)</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate (legfrissebb fordítások)</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="83"/>
         <source>GitHub (updated daily)</source>
-        <translation type="unfinished"></translation>
+        <translation>GitHub (naponta frissített)</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="90"/>
         <source>Local folder (for offline use)</source>
-        <translation type="unfinished"></translation>
+        <translation>Helyi mappa (internet kapcsolat nélküli használatra)</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="97"/>
         <source>Latest file only:</source>
-        <translation type="unfinished"></translation>
+        <translation>Csak a legújabb fájl:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="107"/>
         <source>Refresh Weblate language list by querying the Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Frissítse az elérhető nyelvek listáját a Weblate szerverről.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="110"/>
         <source>refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>frissítés</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="117"/>
         <source>Select language(s) that will be downloaded and installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Válassza ki a letöltendő és telepítendő nyelveket.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="124"/>
         <source>Folder containing translation (.ts) files.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fordítás (.ts) fájlokat tartalmazó mappa.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="127"/>
@@ -81,113 +81,113 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="139"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If this option is checked then only the latest .ts file in the folder will be used. Enable this option to ignore previously downloaded .ts files are saved in the same folder.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt; Ha ezt az opciót választja, akkor csak a legutóbbi .ts fájlt használjuk a mappában és figyelmen kívül hagyjuk az ugyanabba a mappába korábban letöltött .ts fájlokat.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="157"/>
         <source>Application language:</source>
-        <translation type="unfinished"></translation>
+        <translation>Alkalmazás nyelve:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="170"/>
         <source>Change only takes effect after restarting the application.</source>
-        <translation type="unfinished"></translation>
+        <translation>A változás csak az alkalmazás újraindítása után érvényesül.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="180"/>
         <source>Restart the application to see updated translations on the graphical user interface.</source>
-        <translation type="unfinished"></translation>
+        <translation>Indítsa újra az alkalmazást, hogy a frissített fordításokat lássa a felhasználói felületen.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="183"/>
         <source>Restart the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Indítsa újra az alkalmazást</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="192"/>
         <source>Advanced settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Haladó beállítások</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="204"/>
         <source>Qt &apos;lrelease&apos; tool path:</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt &apos;lrelease&apos; eszköz elérési útja:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="211"/>
         <source>Path to Qt lrelease executable. Leave empty to use default (the one bundled with the Slicer installer or available in system paths).</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt szoftver lrelease alkalmazásának helye. Hagyja üresen, ha az alapértelmezett helyet szeretné használni (amelyik a Slicer telepítő csomagban vagy a rendszeren található).</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="221"/>
         <source>Slicer version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Slicer verzió:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="228"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;master, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;Only used for GitHub.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;main, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;Csak GitHub esetén használva&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;]</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="238"/>
         <source>GitHub repository URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>GitHub projekt URL:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="245"/>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="262"/>
         <source>Github repository that stores the translated strings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Github repository, amely tárolja a fordított szövegeket.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="255"/>
         <source>Weblate download URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate letöltési URL:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="272"/>
         <source>Translation file folders:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fordítási fájl mappák:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="279"/>
         <source>List of folders where the application looks for translations. This is for information only, the paths cannot be changed.</source>
-        <translation type="unfinished"></translation>
+        <translation>A mappák listája, ahol az alkalmazás fordításokat keres. Ez csak információ, a mappák helye nem változtatható.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="288"/>
         <source>Show more...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mutasson többet...</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="298"/>
         <source>Find text</source>
-        <translation type="unfinished"></translation>
+        <translation>Keressen szöveget</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="310"/>
         <source>Enable text finder:</source>
-        <translation type="unfinished"></translation>
+        <translation>Szöveg-kereső engedélyezése:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="317"/>
         <source>If enabled, then the shortcut opens the text finder, which can be used to extract text from the application user interface and open it in the translation website.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ha engedélyezett, akkor a billentyűkombináció megnyitja a szöveg-keresőt, amely kimásolja a talált szöveget az alkalmazás felhasználói felületről és megnyitja a fordítási weboldalon.</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="320"/>
         <source> (shortcut: Ctrl+6)</source>
-        <translation type="unfinished"></translation>
+        <translation> (billentyű kombináció: Ctrl+6)</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="327"/>
         <source>Edited language:</source>
-        <translation type="unfinished"></translation>
+        <translation>Szerkesztett nyelv:</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="22"/>
         <source>Language Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyelvi eszközök</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="29"/>
@@ -195,113 +195,121 @@
 This module can build translation files and install them locally. It is useful for creating and testing translations.
 See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt;extension&apos;s documentation&lt;/a&gt;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Ez a modul képes fordítási fájlokat építeni, és helyileg telepíteni őket. Ez hasznos a fordítások létrehozásához és teszteléséhez.
+További információ a &lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt; kiterjesztés dokumentációjában &lt;/a&gt;.
+</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="39"/>
         <source>
 Developed of this module was partially funded by &lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-language-internationalization-and-usability-improvements/&quot;&gt;CZI EOSS grant&lt;/a&gt;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Ennek a modulnak a kifejlesztését részben a &lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-language-internationalization-and-usability-improvements/&quot;&gt;CZI EOSS pályázat&lt;/a&gt; finanszírozta.
+</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="141"/>
         <source>Failed to extract any text from widget</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült szöveget találni</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="156"/>
         <source>Click on the text to find it on the translation website:
 
 {links}</source>
-        <translation type="unfinished"></translation>
+        <translation>Kattintson a szövegre, hogy megtalálja a fordítási weboldalon:
+
+{links}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="174"/>
         <source>Failed to extract any text from: {object}</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült szöveget találni: {object}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="296"/>
         <source>Failed to retrieve language list from Weblate.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a nyelvlistát letölteni Weblate-ből.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="412"/>
         <source>Update failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>A frissítés nem sikerült.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="439"/>
         <source>Update completed! Select application language and restart the application to see the results.</source>
-        <translation type="unfinished"></translation>
+        <translation>A frissítés sikeresen vefejeződött! Válassza ki az alkalmazás nyelvét, és indítsa újra az alkalmazást az eredmények megtekintéséhez.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="515"/>
         <source>Failed to query list of languages from Weblate ({status_code}:{reason})</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a nyelvek lekérdezése Weblate-ről ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="545"/>
         <source>Failed to query list of components from Weblate ({status_code}:{reason})</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a Weblate komponenseinek lekérdezése ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="584"/>
         <source>No .ts files were found in the specified location.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem találhatóak .ts fájlok a megadott helyen.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="588"/>
         <source>Use translation file: {file}</source>
-        <translation type="unfinished"></translation>
+        <translation>Forrás fordítás fájl: {file}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="613"/>
         <source>Download translations for {component}/{language}...</source>
-        <translation type="unfinished"></translation>
+        <translation>Fordítások letöltése: {komponens}/{nyelv}...</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="622"/>
         <source>Failed to download translation from: {url} -- {text}</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült letölteni a fordítást: {url} - {text}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="623"/>
         <source>Download failed. This component may not have been translated to the selected language.</source>
-        <translation type="unfinished"></translation>
+        <translation>A letöltés nem sikerült. Ezt az összetevőt nem fordították le a kiválasztott nyelvre.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="687"/>
         <location filename="LanguageTools/LanguageTools.py" line="710"/>
         <source>Translation files folder is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>A fordítási fájlok mappája nincs kiválasztva.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="690"/>
         <source>lrelease tool path is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>az lrelease eszköz elérési útja nincs megadva.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="728"/>
         <source>No translation (qm) files were found at {location}</source>
-        <translation type="unfinished"></translation>
+        <translation>Nincsenek fordítási (qm) fájlok találtak a megadott {location} mappában</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="730"/>
         <source>Update successfully completed.
 Installed {count} translation files in {location}.</source>
-        <translation type="unfinished"></translation>
+        <translation>Frissítés sikeresen befejeződött.
+{count} fordítási fájl telepítve a {location} mappába.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="736"/>
         <source>This Slicer version does not support custom viewer fonts.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ez a Slicer verzió nem támogatja az egyéni betűtípusokat.</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="759"/>
         <source>Installed {count} font files in {location}.</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítve {count} betűtípus a {location} mappába.</translation>
     </message>
 </context>
 <context>
