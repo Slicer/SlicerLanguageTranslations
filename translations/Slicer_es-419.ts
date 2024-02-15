4382ca79ba8784a4ba14a6e3c45c12c929191e80
Valeria123Valdes <vvalery041@gmail.com>
2024-02-15 18:59:37 +0000

    Translated using Weblate (Spanish (Latin America))
    Currently translated at 99.7% (5368 of 5382 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/es_419/

--- translations/Slicer_es-419.ts
+++ translations/Slicer_es-419.ts
@@ -18561,7 +18561,7 @@ p, li { white-space: pre-wrap; }
         <location filename="Docs/_extracli/BRAINSTransformConvert_tr.h" line="3"/>
         <location filename="Modules/CLI/ResampleDTIVolume/ResampleDTIVolume_tr.h" line="4"/>
         <source>Utilities</source>
-        <translation>Utilidades</translation>
+        <translation>Servicios</translation>
     </message>
     <message>
         <location filename="Docs/_extracli/BRAINSDeface_tr.h" line="4"/>
