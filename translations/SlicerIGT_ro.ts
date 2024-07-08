dcb982fa030254dc89dda9f8acc0903c9329316c
Horia Chirculescu <horiachirculescu@gmail.com>
2024-07-08 10:15:35 +0000

    Translated using Weblate (Romanian)
    Currently translated at 0.5% (3 of 564 strings)
    
    Translation: 3D Slicer/SlicerIGT
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/slicerigt/ro/

--- translations/SlicerIGT_ro.ts
+++ translations/SlicerIGT_ro.ts
@@ -735,7 +735,7 @@ For help on how to use this module visit: &lt;a href=&apos;https://www.slicerigt
         <location filename="VolumeResliceDriver/qSlicerVolumeResliceDriverModule.cxx" line="63"/>
         <location filename="Watchdog/qSlicerWatchdogModule.cxx" line="148"/>
         <source>IGT</source>
-        <translation type="unfinished"></translation>
+        <translation>IGT</translation>
     </message>
     <message>
         <location filename="Guidelet/GuideletLoadable.py" line="19"/>
@@ -745,7 +745,7 @@ For help on how to use this module visit: &lt;a href=&apos;https://www.slicerigt
     <message>
         <location filename="TextureModel/TextureModel.py" line="22"/>
         <source>Surface Models</source>
-        <translation type="unfinished"></translation>
+        <translation>Modele de suprafață</translation>
     </message>
 </context>
 <context>
