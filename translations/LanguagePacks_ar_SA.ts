3d96b6b40a412c081cb19b3b982fb3a04dd472c5
Mohamed Alalli BILAL <mohamedallalibilal@esp.sn>
2024-09-23 20:50:02 +0000

    Translated using Weblate (Arabic (Saudi Arabia))
    Currently translated at 98.2% (56 of 57 strings)
    
    Translation: 3D Slicer/LanguagePacks
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/languagepacks/ar_SA/

--- translations/LanguagePacks_ar_SA.ts
+++ translations/LanguagePacks_ar_SA.ts
@@ -317,7 +317,7 @@ Installed {count} translation files in {location}.</source>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="23"/>
         <source>Utilities</source>
-        <translation>الأدوات المساعدة</translation>
+        <translation>الأدوات</translation>
     </message>
 </context>
 </TS>
