d03928c8b1f069bdf6a97e1bc5428d682d034462
Mohamed Alalli BILAL <mohamedallalibilal@esp.sn>
2024-09-23 20:50:02 +0000

    Translated using Weblate (Arabic (Saudi Arabia))
    Currently translated at 97.9% (5439 of 5552 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/ar_SA/

--- translations/Slicer_ar-SA.ts
+++ translations/Slicer_ar-SA.ts
@@ -19235,7 +19235,7 @@ p, li { white-space: pre-wrap; }
         <location filename="Docs/_extracli/BRAINSTransformConvert_tr.h" line="3"/>
         <location filename="Modules/CLI/ResampleDTIVolume/ResampleDTIVolume_tr.h" line="4"/>
         <source>Utilities</source>
-        <translation>الأدوات المساعدة</translation>
+        <translation>الأدوات</translation>
     </message>
     <message>
         <location filename="Docs/_extracli/BRAINSDeface_tr.h" line="4"/>
