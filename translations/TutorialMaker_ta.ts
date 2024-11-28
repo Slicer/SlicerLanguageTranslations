93716fb56a07d614703e2b6a1765d99dc6d56aaf
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-11-28 02:25:46 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: 3D Slicer/TutorialMaker
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/tutorialmaker/ta/

--- translations/TutorialMaker_ta.ts
+++ translations/TutorialMaker_ta.ts
@@ -6,14 +6,14 @@
     <message>
         <location filename="TutorialMaker/Resources/UI/WidgetAnnotator.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>வடிவம்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/WidgetAnnotator.ui" line="26"/>
         <location filename="TutorialMaker/Resources/UI/WidgetAnnotator.ui" line="39"/>
         <location filename="TutorialMaker/Resources/UI/WidgetAnnotator.ui" line="52"/>
         <source>PushButton</source>
-        <translation type="unfinished"></translation>
+        <translation>புச்பட்டன்</translation>
     </message>
 </context>
 <context>
@@ -21,7 +21,7 @@
     <message>
         <location filename="TutorialMaker/Lib/GitTools.py" line="33"/>
         <source>Path does not exist</source>
-        <translation type="unfinished"></translation>
+        <translation>பாதை இல்லை</translation>
     </message>
 </context>
 <context>
@@ -29,22 +29,22 @@
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialEditor.ui" line="20"/>
         <source>TutorialEditor</source>
-        <translation type="unfinished"></translation>
+        <translation>டுடோரியல் ஆசிரியர்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialEditor.ui" line="29"/>
         <source>Annotate</source>
-        <translation type="unfinished"></translation>
+        <translation>சிறுகுறிப்பு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialEditor.ui" line="36"/>
         <source>Start Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>பதிவு செய்யத் தொடங்குங்கள்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialEditor.ui" line="46"/>
         <source>Next Widget</source>
-        <translation type="unfinished"></translation>
+        <translation>அடுத்த விட்செட்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/CreateNewTutorial.ui" line="14"/>
@@ -77,22 +77,22 @@
     <message>
         <location filename="TutorialMaker/Lib/TutorialEditor.py" line="17"/>
         <source>Tutorial Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>டுடோரியல் ஆசிரியர்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialEditor.py" line="104"/>
         <source>Stop Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>பதிவு செய்வதை நிறுத்துங்கள்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialEditor.py" line="109"/>
         <source>Start Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>பதிவு செய்யத் தொடங்குங்கள்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialEditor.py" line="154"/>
         <source>Widget Annotator</source>
-        <translation type="unfinished"></translation>
+        <translation>விட்செட் சிறுகுறிப்பு</translation>
     </message>
 </context>
 <context>
@@ -100,87 +100,87 @@
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="124"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>திற</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="125"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>சேமி</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="126"/>
         <source>Undo</source>
-        <translation type="unfinished"></translation>
+        <translation>செயல்தவிர்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="127"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>நீக்கு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="128"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>கூட்டு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="129"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>நகலெடு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="151"/>
         <source>Square</source>
-        <translation type="unfinished"></translation>
+        <translation>நாற்கை</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="155"/>
         <source>Circle</source>
-        <translation type="unfinished"></translation>
+        <translation>வட்டம்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="158"/>
         <source>Click</source>
-        <translation type="unfinished"></translation>
+        <translation>சொடுக்கு செய்க</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="162"/>
         <source>Arrow</source>
-        <translation type="unfinished"></translation>
+        <translation>அம்பு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="166"/>
         <source>Icon</source>
-        <translation type="unfinished"></translation>
+        <translation>படவுரு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="169"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>உரை</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="209"/>
         <source>color</source>
-        <translation type="unfinished"></translation>
+        <translation>நிறம்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="215"/>
         <source> thick.</source>
-        <translation type="unfinished"></translation>
+        <translation> அடர்த்தியான.</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="223"/>
         <source>Fill</source>
-        <translation type="unfinished"></translation>
+        <translation>நிரப்பவும்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="245"/>
         <source>Load icon</source>
-        <translation type="unfinished"></translation>
+        <translation>ஐகானை ஏற்றவும்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="260"/>
         <source>JSON Files (*.json)</source>
-        <translation type="unfinished"></translation>
+        <translation>சாதொபொகு கோப்புகள் (*.JSON)</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="362"/>
@@ -190,7 +190,7 @@
     <message>
         <location filename="TutorialMaker/Lib/TutorialGUI.py" line="518"/>
         <source>Add a tittle here</source>
-        <translation type="unfinished"></translation>
+        <translation>இங்கே ஒரு உறுதியைச் சேர்க்கவும்</translation>
     </message>
 </context>
 <context>
@@ -198,42 +198,42 @@
     <message>
         <location filename="TutorialMaker/Resources/UI/ScreenshotAnnotator.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>வடிவம்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="38"/>
         <source>Tutorial Making</source>
-        <translation type="unfinished"></translation>
+        <translation>பயிற்சி தயாரித்தல்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="47"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>தொகு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="54"/>
         <source>Load</source>
-        <translation type="unfinished"></translation>
+        <translation>சுமை</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="61"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>சேமி</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="68"/>
         <source>Export Screenshots</source>
-        <translation type="unfinished"></translation>
+        <translation>திரை சாட்களை ஏற்றுமதி செய்யுங்கள்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="84"/>
         <source>Run and Annotate</source>
-        <translation type="unfinished"></translation>
+        <translation>ஓடி சிறுகுறிப்பு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="94"/>
         <source>Test Translation</source>
-        <translation type="unfinished"></translation>
+        <translation>சோதனை மொழிபெயர்ப்பு</translation>
     </message>
     <message>
         <location filename="TutorialMaker/Resources/UI/TutorialMaker.ui" line="104"/>
@@ -243,14 +243,16 @@
     <message>
         <location filename="TutorialMaker/TutorialMaker.py" line="31"/>
         <source>Tutorial Maker</source>
-        <translation type="unfinished"></translation>
+        <translation>பயிற்சி தயாரிப்பாளர்</translation>
     </message>
     <message>
         <location filename="TutorialMaker/TutorialMaker.py" line="46"/>
         <source>
         Development of this module was funded by &lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-for-latin-america-localization-and-outreach/&quot;&gt;CZI EOSS grant&lt;/a&gt;.
         </source>
-        <translation type="unfinished"></translation>
+        <translation>
+        இந்த தொகுதியின் வளர்ச்சியை &lt;a href = &quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-for-latin-america-lacalacation-and-outreach/&quot;&gt; czi eoss மானியம் &lt;/a&gt; ஆல் நிதியளித்தது.
+        </translation>
     </message>
 </context>
 <context>
@@ -260,7 +262,7 @@
         <location filename="TutorialMaker/Lib/painter.py" line="317"/>
         <location filename="TutorialMaker/Lib/painter.py" line="388"/>
         <source>Error: Load an image first.</source>
-        <translation type="unfinished"></translation>
+        <translation>பிழை: முதலில் ஒரு படத்தை ஏற்றவும்.</translation>
     </message>
 </context>
 <context>
