e656d9f4c25b8656b693afc9b274fb5f88635dec
Anonymous <noreply@weblate.org>
2024-11-28 06:06:23 +0000

    Translated using Weblate (Moldavian)
    Currently translated at 0.2% (16 of 5552 strings)
    
    Translation: 3D Slicer/3D Slicer
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/3d-slicer/ro_MD/

--- translations/Slicer_ro_MD.ts
+++ translations/Slicer_ro_MD.ts
@@ -20528,6 +20528,7 @@ If unchecked, the exported dataset will be added to the DICOM database.</transla
         <translation type="unfinished">
             <numerusform></numerusform>
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
@@ -30006,6 +30007,7 @@ This should only used by advanced users who understand the consequences of chang
         <source>%1 extension update is available</source>
         <comment>%1 extension updates are available</comment>
         <translation type="unfinished">
+            <numerusform></numerusform>
             <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
