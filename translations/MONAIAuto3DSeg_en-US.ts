9ba4f1d5339a0758d2b139417f1545fd7ebf21ad
Andras Lasso <lasso@queensu.ca>
2025-06-17 00:46:43 -0400

    Make MONAIAuto3DSeg and TotalSegmentator extensions translatable

---
+++ translations/MONAIAuto3DSeg_en-US.ts
@@ -0,0 +1,250 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MONAIAuto3DSeg</name>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="17"/>
+        <source>Inputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="23"/>
+        <source>Remote processing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="76"/>
+        <source>Server:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="112"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="127"/>
+        <source>Segmentation model:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="136"/>
+        <source>List models that contain all the specified words</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="143"/>
+        <source>Search in full text of the segmentation model description. Uncheck to search only in the model names.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="146"/>
+        <source>Full text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="155"/>
+        <source>Download sample data set for the current segmentation model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="158"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="187"/>
+        <source>Input volume 1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="197"/>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="225"/>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="253"/>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="281"/>
+        <source>vtkMRMLScalarVolumeNode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="215"/>
+        <source>Input volume 2:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="243"/>
+        <source>Input volume 3:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="271"/>
+        <source>Input volume 4:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="302"/>
+        <source>Outputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="311"/>
+        <source>Segmentation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="318"/>
+        <source>This will store the segmentation result.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="321"/>
+        <source>vtkMRMLSegmentationNode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="329"/>
+        <source>Brain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="347"/>
+        <source>Create new segmentation on Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="360"/>
+        <source>Start segmentation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="363"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="376"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="385"/>
+        <source>Use standard segment names:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="392"/>
+        <source>If enabled (default) then segment names are obtained from Slicer standard terminology files. If disabled then internal identifiers are used as segment names.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="405"/>
+        <source>Force to use CPU: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="412"/>
+        <source>Use CPU, even if GPU is available. Useful if the GPU does not have enough memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="422"/>
+        <source>MONAI Python package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="429"/>
+        <source>Force upgrade of MONAI Python package to the version required by this module.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="432"/>
+        <source>Force reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="439"/>
+        <source>Get information on the installed MONAI Python package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="442"/>
+        <source>Get Python package information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="462"/>
+        <source>Show all models:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="469"/>
+        <source>Show all models in &quot;Segmentation model&quot; list, including old versions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="479"/>
+        <source>Manage models:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="488"/>
+        <source>Open the folder that contains all downloaded files for models.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="491"/>
+        <source>Open models cache folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="498"/>
+        <source>Delete all downloaded files for all models. The files will be automatically downloaded again as needed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="501"/>
+        <source>Clear cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="510"/>
+        <source>Auto show 3D:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="517"/>
+        <source>If enabled then the segmentation will be shown in 3D by default (takes extra time).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="533"/>
+        <source>Local segmentation server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="542"/>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="562"/>
+        <source>Log to Console: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="576"/>
+        <source>Log to GUI: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="597"/>
+        <source>Start server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MONAIAuto3DSeg/Resources/UI/MONAIAuto3DSeg.ui" line="607"/>
+        <source>Server address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/TotalSegmentator_en-US.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>TotalSegmentator</name>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="17"/>
+        <source>Inputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="23"/>
+        <source>Input volume:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="30"/>
+        <source>Input abdominal, chest, or whole body CT.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="33"/>
+        <source>vtkMRMLScalarVolumeNode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="51"/>
+        <source>Segmentation task:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="58"/>
+        <source>Choose &quot;total&quot; for segmenting all structures, or any of the more specialized segmentation tasks (that segment only a handful of specific structures in a certain region).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="65"/>
+        <source>Fast:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="74"/>
+        <source>Enable fast option to get less accurate results, faster.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="84"/>
+        <source>fast mode is not available for this task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="87"/>
+        <source>not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="99"/>
+        <source>Outputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="105"/>
+        <source>Segmentation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="112"/>
+        <source>This will store the segmentation result.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="115"/>
+        <source>vtkMRMLSegmentationNode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="123"/>
+        <source>Brain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="141"/>
+        <source>Create new segmentation on Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="157"/>
+        <source>Start segmentation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="160"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="170"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="179"/>
+        <source>Use standard segment names:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="186"/>
+        <source>If enabled (default) then segment names are obtained from Slicer standard terminology files. If disabled then TotalSegmentator identifiers are used as segment names.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="199"/>
+        <source>Force to use CPU: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="206"/>
+        <source>Use CPU, even if GPU is available. Useful if the GPU does not have enough memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="216"/>
+        <source>TotalSegmentator Python package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="223"/>
+        <source>Force upgrade of TotalSegmentator Python package to the version required by this module.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="226"/>
+        <source>Force reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="233"/>
+        <source>TotalSegmentator license: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="240"/>
+        <source>Get metadata information for the TotalSegmentator package currently installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="243"/>
+        <source>Get TotalSegmentator package information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="263"/>
+        <source>Set or update TotalSegmentator license key to allow running tasks that require a license. The registration has to be done only once, the license information is stored persistently in the TotalSegmentation configuration.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="266"/>
+        <source>Set license key</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
