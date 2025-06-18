de52af9ae305a8e763b4622e2201e952e47ba257
avilchisg <hvigady@gmail.com>
2025-06-19 01:05:00 +0200

    Added translation using Weblate (Spanish (Latin America))

---
+++ translations/TotalSegmentator_es_419.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_419">
+<context>
+    <name>TotalSegmentator</name>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="17"/>
+        <source>Inputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="23"/>
+        <source>Input volume:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="30"/>
+        <source>Input abdominal, chest, or whole body CT.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="51"/>
+        <source>Segmentation task:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="58"/>
+        <source>Choose &quot;total&quot; for segmenting all structures, or any of the more specialized segmentation tasks (that segment only a handful of specific structures in a certain region).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="65"/>
+        <source>Fast:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="74"/>
+        <source>Enable fast option to get less accurate results, faster.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="84"/>
+        <source>fast mode is not available for this task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="87"/>
+        <source>not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="99"/>
+        <source>Outputs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="105"/>
+        <source>Segmentation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="112"/>
+        <source>This will store the segmentation result.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="138"/>
+        <source>Create new segmentation on Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="154"/>
+        <source>Start segmentation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="157"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="167"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="176"/>
+        <source>Use standard segment names:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="183"/>
+        <source>If enabled (default) then segment names are obtained from Slicer standard terminology files. If disabled then TotalSegmentator identifiers are used as segment names.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="196"/>
+        <source>Force to use CPU: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="203"/>
+        <source>Use CPU, even if GPU is available. Useful if the GPU does not have enough memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="213"/>
+        <source>TotalSegmentator Python package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="220"/>
+        <source>Force upgrade of TotalSegmentator Python package to the version required by this module.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="223"/>
+        <source>Force reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="230"/>
+        <source>TotalSegmentator license: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="237"/>
+        <source>Get metadata information for the TotalSegmentator package currently installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="240"/>
+        <source>Get TotalSegmentator package information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="260"/>
+        <source>Set or update TotalSegmentator license key to allow running tasks that require a license. The registration has to be done only once, the license information is stored persistently in the TotalSegmentation configuration.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="263"/>
+        <source>Set license key</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
