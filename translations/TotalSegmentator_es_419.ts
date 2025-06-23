223e833b396bf0964fc3a137c80067d64fe3facb
Valeria123Valdes <vvalery041@gmail.com>
2025-06-23 20:00:05 +0200

    Translated using Weblate (Spanish (Latin America))
    Currently translated at 96.4% (27 of 28 strings)
    
    Translation: 3D Slicer/TotalSegmentator
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/totalsegmentator/es_419/

--- translations/TotalSegmentator_es_419.ts
+++ translations/TotalSegmentator_es_419.ts
@@ -6,142 +6,142 @@
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="17"/>
         <source>Inputs</source>
-        <translation type="unfinished"></translation>
+        <translation>Entradas</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="23"/>
         <source>Input volume:</source>
-        <translation type="unfinished"></translation>
+        <translation>Volumen de entrada:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="30"/>
         <source>Input abdominal, chest, or whole body CT.</source>
-        <translation type="unfinished"></translation>
+        <translation>TAC abdominal, torácico o de cuerpo entero.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="51"/>
         <source>Segmentation task:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarea de segmentación:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="58"/>
         <source>Choose &quot;total&quot; for segmenting all structures, or any of the more specialized segmentation tasks (that segment only a handful of specific structures in a certain region).</source>
-        <translation type="unfinished"></translation>
+        <translation>Elija &quot;total&quot; para segmentar todas las estructuras, o cualquiera de las tareas de segmentación más especializadas (que segmentan sólo un puñado de estructuras específicas en una región determinada).</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="65"/>
         <source>Fast:</source>
-        <translation type="unfinished"></translation>
+        <translation>Rápido:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="74"/>
         <source>Enable fast option to get less accurate results, faster.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Active la opción rápida para obtener resultados menos precisos, más rápidamente.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="84"/>
         <source>fast mode is not available for this task</source>
-        <translation type="unfinished"></translation>
+        <translation>el modo rápido no está disponible para esta tarea</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="87"/>
         <source>not available</source>
-        <translation type="unfinished"></translation>
+        <translation>no disponible</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="99"/>
         <source>Outputs</source>
-        <translation type="unfinished"></translation>
+        <translation>Salidas</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="105"/>
         <source>Segmentation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Segmentación:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="112"/>
         <source>This will store the segmentation result.</source>
-        <translation type="unfinished"></translation>
+        <translation>Esto almacenará el resultado de la segmentación.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="138"/>
         <source>Create new segmentation on Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Crear una nueva segmentación en Aplicar</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="154"/>
         <source>Start segmentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>Iniciar segmentación.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="157"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicar</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="167"/>
         <source>Advanced</source>
-        <translation type="unfinished"></translation>
+        <translation>Avanzado</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="176"/>
         <source>Use standard segment names:</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilizar nombres de segmento estándar:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="183"/>
         <source>If enabled (default) then segment names are obtained from Slicer standard terminology files. If disabled then TotalSegmentator identifiers are used as segment names.</source>
-        <translation type="unfinished"></translation>
+        <translation>Si está activada (por defecto), los nombres de los segmentos se obtienen de los archivos de terminología estándar de Slicer. Si se desactiva, se utilizan los identificadores de Segmentador total como nombres de segmento.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="196"/>
         <source>Force to use CPU: </source>
-        <translation type="unfinished"></translation>
+        <translation>Forzar el uso de CPU: </translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="203"/>
         <source>Use CPU, even if GPU is available. Useful if the GPU does not have enough memory.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliza el CPU, incluso si el GPU está disponible. Útil si la GPU no tiene suficiente memoria.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="213"/>
         <source>TotalSegmentator Python package:</source>
-        <translation type="unfinished"></translation>
+        <translation>Paquete Python Segmentador total:</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="220"/>
         <source>Force upgrade of TotalSegmentator Python package to the version required by this module.</source>
-        <translation type="unfinished"></translation>
+        <translation>Forzar la actualización del paquete Python de Segmentador total a la versión requerida por este módulo.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="223"/>
         <source>Force reinstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Forzar reinstalación</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="230"/>
         <source>TotalSegmentator license: </source>
-        <translation type="unfinished"></translation>
+        <translation>Licencia de segmentador total: </translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="237"/>
         <source>Get metadata information for the TotalSegmentator package currently installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Obtener información de metadatos para el paquete Segmentador total actualmente instalado</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="240"/>
         <source>Get TotalSegmentator package information</source>
-        <translation type="unfinished"></translation>
+        <translation>Obtener información sobre el paquete Segmentador total</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="260"/>
         <source>Set or update TotalSegmentator license key to allow running tasks that require a license. The registration has to be done only once, the license information is stored persistently in the TotalSegmentation configuration.</source>
-        <translation type="unfinished"></translation>
+        <translation>Establezca o actualice la clave de licencia de Segmentator total para permitir la ejecución de tareas que requieran una licencia. El registro debe realizarse una sola vez, la información de la licencia se almacena de forma persistente en la configuración de Segmentación total.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/Resources/UI/TotalSegmentator.ui" line="263"/>
         <source>Set license key</source>
-        <translation type="unfinished"></translation>
+        <translation>Establecer la clave de licencia</translation>
     </message>
 </context>
 </TS>
