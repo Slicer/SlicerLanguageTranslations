35361a45c14c5b4ad7be377d4e5c23af86a45b22
Valeria123Valdes <vvalery041@gmail.com>
2025-07-10 09:46:31 +0200

    Translated using Weblate (Spanish (Latin America))
    Currently translated at 97.6% (83 of 85 strings)
    
    Translation: 3D Slicer/TotalSegmentator
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/totalsegmentator/es_419/

--- translations/TotalSegmentator_es_419.ts
+++ translations/TotalSegmentator_es_419.ts
@@ -311,27 +311,27 @@ Pulsar OK para reiniciar.</translation>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="740"/>
         <source>- Installing {requirement}...</source>
-        <translation>- Instalando {requisito}...</translation>
+        <translation>- Instalando {requerimiento}...</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="801"/>
         <source>This module requires installation of additional Python packages. Installation needs network connection and may take several minutes. Click OK to proceed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Este módulo requiere la instalación de paquetes Python adicionales. La instalación requiere conexión a la red y puede tardar varios minutos. Dar clic en OK para continuar.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="802"/>
         <source>Confirm Python package installation</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmar la instalación del paquete Python</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="803"/>
         <source>Python packages that will be installed: {package_list}</source>
-        <translation type="unfinished"></translation>
+        <translation>Paquetes de Python que se instalarán: {lista_paquetes}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="809"/>
         <source>PyTorch Python package is required. Installing... (it may take several minutes)</source>
-        <translation type="unfinished"></translation>
+        <translation>Se requiere el paquete PyTorch Python. Instalación... (puede tardar varios minutos)</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="823"/>
@@ -341,120 +341,122 @@ Pulsar OK para reiniciar.</translation>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="845"/>
         <source>This module requires TotalSegmentator Python package update.</source>
-        <translation type="unfinished"></translation>
+        <translation>Este módulo requiere la actualización del paquete Segmentador total de Python.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="846"/>
         <source>Currently installed: {downloadUrl}
 
 Required: {requiredUrl}</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualmente instalado: {descargarUrl}
+
+Requerido: {requerirUrl}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="853"/>
         <source>TotalSegmentator Python package is required. Installing it from {downloadUrl}... (it may take several minutes)</source>
-        <translation type="unfinished"></translation>
+        <translation>Se requiere el paquete Segmentador total de Python. Instalándolo desde {descargaUrl}... (puede tardar varios minutos)</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="865"/>
         <source>TotalSegmentator installation completed successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>La instalación del Segmentador total se ha completado correctamente.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="928"/>
         <source>Setting license...</source>
-        <translation type="unfinished"></translation>
+        <translation>Establecer licencia...</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="948"/>
         <source>License has been successfully set.</source>
-        <translation type="unfinished"></translation>
+        <translation>La licencia se ha configurado correctamente.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="950"/>
         <source>This license update requires a 3D Slicer restart.</source>
         <comment>Press OK to restart.</comment>
-        <translation type="unfinished"></translation>
+        <translation>Esta actualización de licencia requiere reiniciar 3D Slicer.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="978"/>
         <source>Processing started</source>
-        <translation type="unfinished"></translation>
+        <translation>Procesamiento iniciado</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1002"/>
         <source>No GPU is detected. Switch to &apos;fast&apos; mode to get low-resolution result in a few minutes or compute full-resolution result which may take 5 to 50 minutes (depending on computer configuration)?</source>
-        <translation type="unfinished"></translation>
+        <translation>No se detecta ninguna GPU. ¿Cambiar al modo a &quot;rápido&quot; para obtener un resultado de baja resolución en unos minutos o calcule un resultado de resolución completa que puede tardar entre 5 y 50 minutos (según la configuración del ordenador)?</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1002"/>
         <source>Fast (~2 minutes)</source>
-        <translation type="unfinished"></translation>
+        <translation>Rápido (~2 minutos)</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1003"/>
         <source>Full-resolution (~5 to 50 minutes)</source>
-        <translation type="unfinished"></translation>
+        <translation>Resolución completa (~5 a 50 minutos)</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1011"/>
         <source>You have less than 7 GB of GPU memory available. Enable &apos;fast&apos; mode to ensure segmentation can be completed successfully?</source>
-        <translation type="unfinished"></translation>
+        <translation>Dispone de menos de 7 GB de memoria GPU. ¿Activar el modo &quot;rápido&quot; para asegurar que la segmentación se completa correctamente?</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1059"/>
         <source>Processing completed in {time_elapsed:.2f} seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>Procesamiento completado en {tiempo_transcurrido:.2f} segundos</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1062"/>
         <source>Cleaning up temporary folder...</source>
-        <translation type="unfinished"></translation>
+        <translation>Limpiar la carpeta temporal...</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1066"/>
         <source>Not cleaning up temporary folder: {temp_folder}</source>
-        <translation type="unfinished"></translation>
+        <translation>No se limpia la carpeta temporal: {carpeta_temporal}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1074"/>
         <source>Writing input file to {input_file}</source>
-        <translation type="unfinished"></translation>
+        <translation>Escribir archivo de entrada en {archivo_entrada}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1118"/>
         <source>Creating segmentations with TotalSegmentator AI...</source>
-        <translation type="unfinished"></translation>
+        <translation>Creación de segmentaciones con IA Segmentador total...</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1119"/>
         <source>Total Segmentator arguments: {options}</source>
-        <translation type="unfinished"></translation>
+        <translation>Parámetros del Segmentador total: {opciones}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1124"/>
         <source>Importing segmentation results...</source>
-        <translation type="unfinished"></translation>
+        <translation>Importar resultados de segmentación...</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1168"/>
         <source>Importing candidate {segment_name}</source>
-        <translation type="unfinished"></translation>
+        <translation>Importando candidato {nombre_segmento}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1171"/>
         <source>Path {segment_name} not exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>La ruta {nombre_del_segmento} no existe.</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1181"/>
         <source>Importing subset {segment_name}</source>
-        <translation type="unfinished"></translation>
+        <translation>Importando subconjunto {nombre_segmento}</translation>
     </message>
     <message>
         <location filename="TotalSegmentator/TotalSegmentator.py" line="1188"/>
         <source>{segment_name} not found.</source>
-        <translation type="unfinished"></translation>
+        <translation>{nombre_del_segmento} no encontrado.</translation>
     </message>
 </context>
 <context>
