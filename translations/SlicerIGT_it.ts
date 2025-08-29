0f344e25e62528fbb3eb138fbaa4f20200356e58
EliSchumy07 <eliseoschumacher@bluewin.ch>
2025-08-29 23:36:30 +0200

    Translated using Weblate (Italian)
    Currently translated at 6.3% (38 of 599 strings)
    
    Translation: 3D Slicer/SlicerIGT
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/slicerigt/it/

--- translations/SlicerIGT_it.ts
+++ translations/SlicerIGT_it.ts
@@ -279,7 +279,7 @@ This work is part of SparKit project, funded by Cancer Care Ontario (CCO)&apos;s
     <message>
         <location filename="ModelRegistration/ModelRegistration.py" line="104"/>
         <source>Run the algorithm.</source>
-        <translation type="unfinished">Esegui l&apos;algoritmo.</translation>
+        <translation>Esegui l&apos;algoritmo.</translation>
     </message>
     <message>
         <location filename="ModelRegistration/ModelRegistration.py" line="112"/>
@@ -299,7 +299,7 @@ This work is part of SparKit project, funded by Cancer Care Ontario (CCO)&apos;s
     <message>
         <location filename="ModelRegistration/ModelRegistration.py" line="145"/>
         <source>Number of iterations:</source>
-        <translation type="unfinished">Numero di iterazioni:</translation>
+        <translation>Numero di iterazioni:</translation>
     </message>
     <message>
         <location filename="ModelRegistration/ModelRegistration.py" line="270"/>
@@ -1760,7 +1760,7 @@ For help on how to use this module visit: &lt;a href=&apos;https://www.slicerigt
     <message>
         <location filename="PivotCalibration/Resources/UI/qSlicerPivotCalibrationModule.ui" line="408"/>
         <source>Settings</source>
-        <translation type="unfinished">Impostazioni</translation>
+        <translation>Impostazioni</translation>
     </message>
     <message>
         <location filename="PivotCalibration/Resources/UI/qSlicerPivotCalibrationModule.ui" line="417"/>
