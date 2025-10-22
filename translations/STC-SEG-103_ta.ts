cc341507735f8ee17d3035fe43d6730c2c6e3a29
தமிழ்நேரம் <tamilneram247@gmail.com>
2025-10-22 18:46:56 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: 3D Slicer/STC-SEG-103
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-seg-103/ta/

--- translations/STC-SEG-103_ta.ts
+++ translations/STC-SEG-103_ta.ts
@@ -8,7 +8,8 @@
         <source>Click on Add Data in the Welcome
 to Slicer module</source>
         <extracomment>1_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>வெல்கம் என்பதில் டேட்டாவைச் சேர் என்பதைக் சொடுக்கு செய்யவும் 
+ச்லைசர் தொகுதிக்கு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
@@ -16,14 +17,17 @@ to Slicer module</source>
 and browse to the location of the
 Slicer datasets</source>
         <extracomment>2_2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>சேர்க்க கோப்பகத்தைத் தேர்ந்தெடு என்பதைக் சொடுக்கு செய்யவும் 
+மற்றும் இருப்பிடத்தை உலாவவும் 
+ச்லைசர் தரவுத்தொகுப்புகள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
         <source>Slicer loads the prostate MRI
 datase</source>
         <extracomment>3_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் புரோச்டேட் எம்ஆர்ஐயை ஏற்றுகிறது 
+தரவுத்தொகுப்பு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -35,20 +39,27 @@ the category Segmentation.
 Select the MONAI Auto3DSeg
 module</source>
         <extracomment>4_3_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>இதில் வெல்கம் டு ச்லைசரை சொடுக்கு செய்யவும் 
+தொகுதிகள் பட்டியல் மற்றும் உலாவவும் 
+
+வகை பிரிவு. 
+
+MONAI Auto3DSeg ஐத் தேர்ந்தெடுக்கவும் 
+தொகுதி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>Enter the models name Prostate in
 the Segmentation model menu</source>
         <extracomment>5_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மாடல்களின் பெயரை புரோச்டேட் உள்ளிடவும் 
+பிரிவு மாதிரி பட்டியல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
         <source>Select the model Prostate - Multisequence</source>
         <extracomment>6_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ப்ரோச்டேட் - மல்டிசீக்வென்ச் மாதிரியைத் தேர்ந்தெடுக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -56,33 +67,37 @@ the Segmentation model menu</source>
 prostate-01-adc and he Input ADC
 volume msd-prostate-01-adc</source>
         <extracomment>7_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>உள்ளீடு T2 தொகுதி msd-ஐ உள்ளிடவும் 
+prostate-01-adc மற்றும் he Input ADC 
+தொகுதி msd-prostate-01-adc</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
         <source>Select Create new segmentation on
 Apply</source>
         <extracomment>7_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>புதிய பிரிவை உருவாக்கு என்பதைத் தேர்ந்தெடுக்கவும் 
+விண்ணப்பிக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>Click on Apply</source>
         <extracomment>7_0_ArrowText_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>விண்ணப்பிக்கவும் என்பதைக் சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
         <source>Slicer stars the inference</source>
         <extracomment>8_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் அனுமானத்தை நட்சத்திரமாக்குகிறது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
         <source>Slicer shows the result of the AI-
 based prostate segmentation</source>
         <extracomment>9_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் AI இன் முடிவைக் காட்டுகிறது. 
+அடிப்படையிலான புரோச்டேட் பிரிவு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
@@ -102,7 +117,21 @@ Click on Choose File(s) to Add and
 browse to the location of the Slicer
 datasets</source>
         <extracomment>10_2_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>வெல்கம் என்பதில் டேட்டாவைச் சேர் என்பதைக் சொடுக்கு செய்யவும் 
+ச்லைசர் தொகுதிக்கு 
+
+
+
+
+
+
+
+
+
+
+சேர்ப்பதற்கு கோப்பு(களை) தேர்ந்தெடு என்பதைக் சொடுக்கு செய்யவும் 
+ச்லைசரின் இடத்திற்கு உலாவவும் 
+தரவுத்தொகுப்புகள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
@@ -110,25 +139,28 @@ datasets</source>
 model’s name Brain Tumor Segmentation in the
 Segmentation model menu</source>
         <extracomment>12_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>MONAIAauto3DSeg தொகுதியைத் தேர்ந்தெடுத்து உள்ளிடவும் 
+மாடலின் பெயர் Brain Tumor Segmentation இல் 
+பிரிவு மாதிரி பட்டியல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Select the model Brain Tumor Segmentation
 (BRATS) GLI</source>
         <extracomment>12_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூளை கட்டி பிரிவின் மாதிரியைத் தேர்ந்தெடுக்கவும் 
+(பிராட்ச்) சி.எல்.ஐ</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
         <source>Enter the input volumes as follows:</source>
         <extracomment>13_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>உள்ளீட்டு தொகுதிகளை பின்வருமாறு உள்ளிடவும்:</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="39"/>
         <source>Slicer starts running the inference task);</source>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் அனுமானப் பணியை இயக்கத் தொடங்குகிறார்);</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
@@ -136,7 +168,9 @@ Segmentation model menu</source>
 select the Patient patient1 and
 click on Load to load it in Slicer</source>
         <extracomment>16_2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>சேர் DICOM தரவு தொகுதியில், 
+நோயாளி நோயாளி1 மற்றும் 
+ச்லைசரில் ஏற்றுவதற்கு ஏற்ற என்பதைக் சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -145,14 +179,18 @@ model’s name Whole Body Segmentation in the
 
 Segmentation model menu</source>
         <extracomment>17_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>MONAIAauto3DSeg தொகுதியைத் தேர்ந்தெடுத்து உள்ளிடவும் 
+முழு உடல் பிரிவு மாடலின் பெயர் 
+
+பிரிவு மாதிரிப் பட்டியல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
         <source>Select the model Whole Body Segmentation
 TS1 - quick</source>
         <extracomment>17_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>முழு உடல் பிரிவின் மாதிரியைத் தேர்ந்தெடுக்கவும் 
+TS1 - விரைவானது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
@@ -161,13 +199,16 @@ TS1 - quick</source>
 Click on Create new Segmentation on Apply
 Click on Apply to start the segmentation</source>
         <extracomment>18_0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>உள்ளீடு தொகுதி 6 ஐத் தேர்ந்தெடுக்கவும்: CT_Thorax_Abdomen, 
+
+Apply என்பதில் Create புதிய Segmentation என்பதில் சொடுக்கு செய்யவும் 
+பிரிவைத் தொடங்க விண்ணப்பிக்கவும் என்பதைக் சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>Slicer starts the segmentation</source>
         <extracomment>19_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் பிரிவைத் தொடங்குகிறது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="54"/>
@@ -175,7 +216,9 @@ Click on Apply to start the segmentation</source>
 
 the segments 3D view</source>
         <extracomment>20_0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>பார்க்க 3D ஐக் சொடுக்கு செய்க 
+
+பிரிவுகள் 3D காட்சி</translation>
     </message>
 </context>
 </TS>
