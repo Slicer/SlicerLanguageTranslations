5b7d5e6e7bff0f78d65c5ed13ed9afd66c049b47
Francisco Serrador <fserrador@gmail.com>
2025-12-19 19:31:18 +0100

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: 3D Slicer/STC-SEG-103
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-seg-103/es/

--- translations/STC-SEG-103_es.ts
+++ translations/STC-SEG-103_es.ts
@@ -8,7 +8,8 @@
         <source>Click on Add Data in the Welcome
 to Slicer module</source>
         <extracomment>1_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse sobre Añadir Datos en el Bienvenido
+en el módulo Slicer</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
@@ -16,14 +17,17 @@ to Slicer module</source>
 and browse to the location of the
 Slicer datasets</source>
         <extracomment>2_2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse en Elegir Directorio para añadir y
+explorar el lugar de las bases de datos
+Slicer</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
         <source>Slicer loads the prostate MRI
 datase</source>
         <extracomment>3_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer carga el conjunto de datos de
+resonancia magnética de próstata</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -35,20 +39,27 @@ the category Segmentation.
 Select the MONAI Auto3DSeg
 module</source>
         <extracomment>4_3_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse sobre Bienvenido a Rebanada en
+el menú de Módulos y examinar a la
+
+categoría Segmentación.
+
+Seleccione el módulo
+ MONAI Auto3DSeg</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>Enter the models name Prostate in
 the Segmentation model menu</source>
         <extracomment>5_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Introduzca el nombre Próstata del modelo
+en el menú de modelo Segmentación</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
         <source>Select the model Prostate - Multisequence</source>
         <extracomment>6_0_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione el modelo Próstata – Multisecuencia</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -56,33 +67,37 @@ the Segmentation model menu</source>
 prostate-01-adc and he Input ADC
 volume msd-prostate-01-adc</source>
         <extracomment>7_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Introduzca el volumen T2 de entrada
+msd-prostate-01-adc e introduzca el
+volumen ADC msd-prostate-01-adc</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
         <source>Select Create new segmentation on
 Apply</source>
         <extracomment>7_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione Crear segmentación nueva
+en Aplicar</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>Click on Apply</source>
         <extracomment>7_0_ArrowText_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse sobre Aplicar</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
         <source>Slicer stars the inference</source>
         <extracomment>8_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>El rebanador inicia la inferencia</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
         <source>Slicer shows the result of the AI-
 based prostate segmentation</source>
         <extracomment>9_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>El rebanador muestra el resultado del
+IA-basada en segmentación de próstata</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
@@ -102,7 +117,21 @@ Click on Choose File(s) to Add and
 browse to the location of the Slicer
 datasets</source>
         <extracomment>10_2_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse en Añadir Datos en el módulo
+Bienvenido al módulo Rebanador
+
+
+
+
+
+
+
+
+
+
+Pulse sobre Elegir Archivo(s) para Añadir y
+examinar al lugar del conjunto de datos de
+la base de datos Rebanador</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
@@ -110,25 +139,28 @@ datasets</source>
 model’s name Brain Tumor Segmentation in the
 Segmentation model menu</source>
         <extracomment>12_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione el módulo MONAIAuto3DSeg e introduzca
+el nombre de modelo Segmentación de Tumor Cerebral
+en el menú del modelo Segmentación</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Select the model Brain Tumor Segmentation
 (BRATS) GLI</source>
         <extracomment>12_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Selecciona el modelo Segmentación de Tumor Cerebral
+(BRATS) GLI</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
         <source>Enter the input volumes as follows:</source>
         <extracomment>13_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Introduzca los volúmenes de entrada como sigue:</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="39"/>
         <source>Slicer starts running the inference task);</source>
-        <translation type="unfinished"></translation>
+        <translation>La rebanada inicia ejecutando el tarea inferencia);</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
@@ -136,7 +168,10 @@ Segmentation model menu</source>
 select the Patient patient1 and
 click on Load to load it in Slicer</source>
         <extracomment>16_2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>En el módulo Añadir Datos DICOM,
+selecciona el Paciente paciente1 y
+pulsa en Cargar para cargarlo en el
+Rebanador</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -145,14 +180,18 @@ model’s name Whole Body Segmentation in the
 
 Segmentation model menu</source>
         <extracomment>17_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione el módulo MONAIAuto3DSeg e introduzca
+el nombre del modelo Segmentación de Cuerpo Completo
+
+en el menú Segmentación del modelo</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
         <source>Select the model Whole Body Segmentation
 TS1 - quick</source>
         <extracomment>17_0_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione el modelo Segmentación de Cuerpo Completo
+TS1 - rápido</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
@@ -161,13 +200,16 @@ TS1 - quick</source>
 Click on Create new Segmentation on Apply
 Click on Apply to start the segmentation</source>
         <extracomment>18_0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione la entrada del Volumen 6: TC_Tórax_Abdomen.
+
+Pulse en Crear segmentación nueva sobre Aplicar
+Pulse en Aplicar para iniciar la segmentación</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>Slicer starts the segmentation</source>
         <extracomment>19_0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Rebanador comienza la segmentación</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="54"/>
@@ -175,7 +217,9 @@ Click on Apply to start the segmentation</source>
 
 the segments 3D view</source>
         <extracomment>20_0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pulse sobre Mostrar 2D para
+
+ver el segmento en vista 3D</translation>
     </message>
 </context>
 </TS>
