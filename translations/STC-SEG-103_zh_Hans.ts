637e113aee6cbdda3a77f820bb59d6ec5c6ff52c
ribong <154654268@aliyun.com>
2026-01-17 13:39:09 +0100

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: 3D Slicer/STC-SEG-103
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-seg-103/zh_Hans/

--- translations/STC-SEG-103_zh_Hans.ts
+++ translations/STC-SEG-103_zh_Hans.ts
@@ -4,29 +4,33 @@
 <context>
     <name>Main</name>
     <message>
+        <location filename="text_dict_default.json" line="8"/>
         <source>Click on Add Data in the Welcome
 to Slicer module</source>
         <extracomment>1_0_ArrowText_0</extracomment>
-        <translation type="vanished">在“欢迎使用Slicer”模块中
+        <translation>在“欢迎使用Slicer”模块中
 单击“添加数据”</translation>
     </message>
     <message>
+        <location filename="text_dict_default.json" line="10"/>
         <source>Click on Choose Directory to add
 and browse to the location of the
 Slicer datasets</source>
         <extracomment>2_2_TextBox_1</extracomment>
-        <translation type="vanished">点击选择文件夹来添加
+        <translation>点击选择文件夹来添加
 并浏览到 
 Slicer 数据集 的位置</translation>
     </message>
     <message>
+        <location filename="text_dict_default.json" line="12"/>
         <source>Slicer loads the prostate MRI
 datase</source>
         <extracomment>3_0_TextBox_0</extracomment>
-        <translation type="vanished">Slicer 加载前列腺MRI
+        <translation>Slicer 加载前列腺MRI
 数据集</translation>
     </message>
     <message>
+        <location filename="text_dict_default.json" line="14"/>
         <source>Click on Welcome to Slicer in the
 Modules menu and browse to
 
@@ -35,7 +39,7 @@ the category Segmentation.
 Select the MONAI Auto3DSeg
 module</source>
         <extracomment>4_3_TextBox_1</extracomment>
-        <translation type="vanished">在“模块”菜单中
+        <translation>在“模块”菜单中
 点击“欢迎来到Slicer”，
 
 找到“分割”。
@@ -44,180 +48,177 @@ module</source>
 模块</translation>
     </message>
     <message>
-        <location filename="text_dict_default.json" line="24"/>
-        <source>Select the model Prostate - Multisequence</source>
-        <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="8"/>
-        <source>AIBasedSegmentationIn3DSlicer</source>
-        <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="10"/>
-        <source>Sonia Pujol, Ph.D.</source>
-        <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="12"/>
-        <source>30/06/2025</source>
-        <extracomment>0_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="14"/>
-        <source>AI - based Segmentation in 3D Slicer</source>
-        <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="text_dict_default.json" line="16"/>
-        <source>Click on Add Data in the Welcome to 	Slicer module</source>
-        <extracomment>1_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Enter the models name Prostate in
+the Segmentation model menu</source>
+        <extracomment>5_0_ArrowText_0</extracomment>
+        <translation>在
+分割模型菜单中输入模型名称 Prostate</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
-        <source>Slicer loads the prostate MRI dataset</source>
-        <extracomment>2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Select the model Prostate - Multisequence</source>
+        <extracomment>6_0_ArrowText_0</extracomment>
+        <translation>选择模型 前列腺 - 多序列</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
-        <source>Click on Welcome to Slicer in the
-Modules’ menu and browse to
- the category Segmentation.</source>
-        <extracomment>3_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Enter the Input T2 volume msd-
+prostate-01-adc and he Input ADC
+volume msd-prostate-01-adc</source>
+        <extracomment>7_0_TextBox_0</extracomment>
+        <translation>输入T2体积msd-prostate-01-adc
+，输入ADC
+体积msd-prostate-01-adc</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
-        <source>Enter the model’s name Prostate in
-the Segmentation model menu</source>
-        <extracomment>4_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Select Create new segmentation on
+Apply</source>
+        <extracomment>7_0_ArrowText_1</extracomment>
+        <translation>在应用中
+选择创建新细分</translation>
+    </message>
+    <message>
+        <location filename="text_dict_default.json" line="24"/>
+        <source>Click on Apply</source>
+        <extracomment>7_0_ArrowText_2</extracomment>
+        <translation>点击“应用”</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
-        <source>Enter the Input T2 volume msdprostate-01-adc</source>
-        <extracomment>5_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Slicer stars the inference</source>
+        <extracomment>8_0_TextBox_0</extracomment>
+        <translation>切片器为推断加星</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
-        <source>And the Input ADC volume msd-prostate-01-adc</source>
-        <extracomment>6_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Slicer shows the result of the AI-
+based prostate segmentation</source>
+        <extracomment>9_0_TextBox_0</extracomment>
+        <translation>切片器显示基于AI
+的前列腺分割结果</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
-        <source>Click on Create new segmentation on
-Apply</source>
-        <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Click on Add Data in the Welcome
+to Slicer module
+
+
+
+
+
+
+
+
+
+
+Click on Choose File(s) to Add and
+browse to the location of the Slicer
+datasets</source>
+        <extracomment>10_2_TextBox_2</extracomment>
+        <translation>点击“欢迎
+使用切片软件”模块
+
+
+
+
+
+
+
+
+
+
+中的添加数据，点击选择文件以添加并
+浏览切片软件
+数据集的位置</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
-        <location filename="text_dict_default.json" line="57"/>
-        <source>Slicer starts the inference</source>
-        <extracomment>7_TextBox_0
-----------
-17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="34"/>
-        <source>Slicer shows the result of the AIbased prostate segmentation</source>
-        <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="36"/>
-        <source>Load dataset4_BrainMRI_Glioma</source>
-        <extracomment>9_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="38"/>
         <source>Select the module MONAIAuto3DSeg and enter the
 model’s name Brain Tumor Segmentation in the
 Segmentation model menu</source>
-        <extracomment>10_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <extracomment>12_0_TextBox_0</extracomment>
+        <translation>选择模块 MONAIAuto3DSeg，并在“分割”模型菜单中输入
+模型名称“脑肿瘤分割
+”</translation>
     </message>
     <message>
-        <location filename="text_dict_default.json" line="40"/>
+        <location filename="text_dict_default.json" line="34"/>
         <source>Select the model Brain Tumor Segmentation
 (BRATS) GLI</source>
-        <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <extracomment>12_0_ArrowText_1</extracomment>
+        <translation>选择脑肿瘤分割
+（BRATS）GLI模型</translation>
     </message>
     <message>
-        <location filename="text_dict_default.json" line="42"/>
-        <source>Enter the input volumes as follows:
-Input T2F volume: BraTS-GLI_00005-000-t12f
-Input T1C volume: BraTS-GLI_00005-000-t1c
-Input T1N volume: BraTS-GLI_00005-000-t1n
-Input T2W volume: BraTS-GLI_00005-000-t2w
-Click on Create new Segmentation on Apply
-Click on Apply to start the segmentation</source>
-        <extracomment>11_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <location filename="text_dict_default.json" line="36"/>
+        <source>Enter the input volumes as follows:</source>
+        <extracomment>13_0_TextBox_0</extracomment>
+        <translation>输入卷的输入方式如下：</translation>
+    </message>
+    <message>
+        <location filename="text_dict_default.json" line="39"/>
+        <source>Slicer starts running the inference task);</source>
+        <translation>切片器开始执行推理任务）;</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
-        <source>Slicer starts running the inference task</source>
-        <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>In the Add DICOM Data module,
+select the Patient patient1 and
+click on Load to load it in Slicer</source>
+        <extracomment>16_2_TextBox_0</extracomment>
+        <translation>在添加DICOM数据模块中，
+选择患者1，点击
+加载以在切片软件中加载</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
-        <source>Once the segmentation is done, ‘Processing finished’
-appears in the Slicer GUI</source>
-        <extracomment>13_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Select the module MONAIAuto3DSeg and enter the
+model’s name Whole Body Segmentation in the
+
+Segmentation model menu</source>
+        <extracomment>17_0_TextBox_0</extracomment>
+        <translation>选择模块 MONAIAuto3DSeg，并在“分段模型”菜单中输入
+模型名称“全体分段
+
+”</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
-        <source>In the Add DICOM Data module,
-select the Patient patient1 and
-double click on the image
-CT_Thorax_Abdomen to load it in
-Slicer</source>
-        <extracomment>14_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <source>Select the model Whole Body Segmentation
+TS1 - quick</source>
+        <extracomment>17_0_ArrowText_1</extracomment>
+        <translation>选择模型 全身分段
+TS1 - 快速</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
-        <source>Select the module MONAIAuto3DSeg and enter the
-model’s name Whole Body Segmentation in the
-Segmentation model menu);</source>
-        <extracomment>15_ArrowText_0 15_ArrowText_1</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="text_dict_default.json" line="55"/>
         <source>Select the input Volume 6: CT_Thorax_Abdomen,
+
 Click on Create new Segmentation on Apply
 Click on Apply to start the segmentation</source>
-        <extracomment>16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <extracomment>18_0_TextBox_3</extracomment>
+        <translation>选择输入 第6卷：CT_Thorax_Abdomen，
+
+点击“创建新分段”，点击应用
+，点击“应用”开始分段</translation>
     </message>
     <message>
-        <location filename="text_dict_default.json" line="59"/>
-        <source>Slicer displays the result of the AI-based segmentation
-using the Whole Body Segmentation TS1- quick</source>
-        <extracomment>18_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <location filename="text_dict_default.json" line="52"/>
+        <source>Slicer starts the segmentation</source>
+        <extracomment>19_0_TextBox_0</extracomment>
+        <translation>切片器开始分段</translation>
     </message>
     <message>
-        <location filename="text_dict_default.json" line="61"/>
-        <source>Acknowledgements</source>
-        <extracomment>19_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <location filename="text_dict_default.json" line="54"/>
+        <source>Click on Show 3D to see
+
+the segments 3D view</source>
+        <extracomment>20_0_TextBox_1</extracomment>
+        <translation>点击“显示3D”查看
+
+segment的3D视图</translation>
     </message>
 </context>
 </TS>
