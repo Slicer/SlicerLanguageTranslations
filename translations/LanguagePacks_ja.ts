4f7410282f02ca75825a895deab954e0d743943d
saki_imai <sachiko_imanishi@outlook.jp>
2026-01-18 09:32:57 +0100

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (57 of 57 strings)
    
    Translation: 3D Slicer/LanguagePacks
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/languagepacks/ja/

--- translations/LanguagePacks_ja.ts
+++ translations/LanguagePacks_ja.ts
@@ -51,132 +51,132 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="253"/>
         <source>Refresh Weblate language list by querying the Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate サーバーにクエリを実行して、Weblate 言語リストを更新します。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="256"/>
         <source>refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>リフレッシュする</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="263"/>
         <source>Select language(s) that will be downloaded and installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>ダウンロードしてインストールする言語を選択します。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="270"/>
         <source>Folder containing translation (.ts) files.</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳 (.ts) ファイルを含むフォルダー。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="285"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If this option is checked then only the latest .ts file in the folder will be used. Enable this option to ignore previously downloaded .ts files are saved in the same folder.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;このオプションをオンにすると、フォルダー内の最新の .ts ファイルのみが使用されます。以前にダウンロードした .ts ファイルが同じフォルダーに保存されることを無視するには、このオプションを有効にします。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="151"/>
         <source>Application language:</source>
-        <translation type="unfinished"></translation>
+        <translation>アプリケーション言語:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="164"/>
         <source>Change only takes effect after restarting the application.</source>
-        <translation type="unfinished"></translation>
+        <translation>変更はアプリケーションを再起動した後にのみ有効になります。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="174"/>
         <source>Restart the application to see updated translations on the graphical user interface.</source>
-        <translation type="unfinished"></translation>
+        <translation>アプリケーションを再起動すると、グラフィカル ユーザー インターフェイスで更新された翻訳が表示されます。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="177"/>
         <source>Restart the application</source>
-        <translation type="unfinished"></translation>
+        <translation>アプリケーションを再起動します</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="27"/>
         <source>Advanced settings</source>
-        <translation type="unfinished"></translation>
+        <translation>詳細設定</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="39"/>
         <source>Qt &apos;lrelease&apos; tool path:</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt &apos;lrelease&apos; ツールパス:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="46"/>
         <source>Path to Qt lrelease executable. Leave empty to use default (the one bundled with the Slicer installer or available in system paths).</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt lrelease 実行可能ファイルへのパス。デフォルト (スライサー インストーラーにバンドルされているもの、またはシステム パスで利用可能なもの) を使用するには、空のままにします。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="59"/>
         <source>Slicer version:</source>
-        <translation type="unfinished"></translation>
+        <translation>スライサーのバージョン:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="66"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;master, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;Only used for GitHub.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;マスター、4.13、4.14、...&lt;/p&gt;&lt;p&gt;GitHub でのみ使用されます。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="76"/>
         <source>GitHub repository URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>GitHub リポジトリ URL:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="83"/>
         <source>Github repository that stores the translated strings.</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳された文字列を保存する Github リポジトリ。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="93"/>
         <source>Weblate download URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate のダウンロード URL:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="100"/>
         <source>Weblate project that stores the translated strings.</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳された文字列を保存する Weblate プロジェクト。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="110"/>
         <source>Translation file folders:</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳ファイルのフォルダー:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="117"/>
         <source>List of folders where the application looks for translations. This is for information only, the paths cannot be changed.</source>
-        <translation type="unfinished"></translation>
+        <translation>アプリケーションが翻訳を検索するフォルダーのリスト。これは情報提供のみを目的としており、パスを変更することはできません。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="126"/>
         <source>Show more...</source>
-        <translation type="unfinished"></translation>
+        <translation>もっと見る...</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="301"/>
         <source>Find text</source>
-        <translation type="unfinished"></translation>
+        <translation>テキストの検索</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="313"/>
         <source>Enable text finder:</source>
-        <translation type="unfinished"></translation>
+        <translation>テキストファインダーを有効にする:</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="322"/>
         <source>If enabled, then the shortcut opens the text finder, which can be used to extract text from the application user interface and open it in the translation website.</source>
-        <translation type="unfinished"></translation>
+        <translation>有効にすると、ショートカットによってテキスト ファインダーが開き、アプリケーションのユーザー インターフェイスからテキストを抽出して翻訳 Web サイトで開くために使用できます。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="347"/>
         <source>Keyboard shortcut:</source>
-        <translation type="unfinished"></translation>
+        <translation>キーボードショートカット:</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="22"/>
         <source>Language Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>言語ツール</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="29"/>
@@ -184,19 +184,24 @@
 This module can build translation files and install them locally. It is useful for creating and testing translations.
 See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt;extension&apos;s documentation&lt;/a&gt;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+このモジュールは翻訳ファイルを構築し、ローカルにインストールできます。翻訳の作成とテストに役立ちます。
+詳細については、&lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt;拡張機能のドキュメント&lt;/a&gt;をご覧ください。
+</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="39"/>
         <source>
 Developed of this module was partially funded by &lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-language-internationalization-and-usability-improvements/&quot;&gt;CZI EOSS grant&lt;/a&gt;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+このモジュールの開発には、&lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-langage-internationalization-and-usability-improvements/&quot;&gt;CZI EOSS 助成金&lt;/a&gt;による資金の一部が提供されました。
+</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="141"/>
         <source>Failed to extract any text from widget</source>
-        <translation type="unfinished"></translation>
+        <translation>ウィジェットからテキストを抽出できませんでした</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="156"/>
@@ -206,94 +211,97 @@ Developed of this module was partially funded by &lt;a href=&quot;https://chanzu
         <oldsource>Click on the text to find it on the translation website:
 
 {links}</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>テキストをクリックすると、翻訳 Web サイト [{language}] で見つかります。
+
+{links}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="174"/>
         <source>Failed to extract any text from: {object}</source>
-        <translation type="unfinished"></translation>
+        <translation>次からテキストを抽出できませんでした: {object}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="287"/>
         <source>Failed to retrieve language list from Weblate.</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate から言語リストを取得できませんでした。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="399"/>
         <source>Update failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>アップデートに失敗しました。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="424"/>
         <source>Update completed! Select application language and restart the application to see the results.</source>
-        <translation type="unfinished"></translation>
+        <translation>アップデート完了！アプリケーション言語を選択し、アプリケーションを再起動して結果を確認します。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="500"/>
         <source>Failed to query list of languages from Weblate ({status_code}:{reason})</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate からの言語リストのクエリに失敗しました ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="533"/>
         <source>Failed to query list of components from Weblate ({status_code}:{reason})</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate からのコンポーネントのリストのクエリに失敗しました ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="572"/>
         <source>No .ts files were found in the specified location.</source>
-        <translation type="unfinished"></translation>
+        <translation>指定された場所に .ts ファイルが見つかりませんでした。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="576"/>
         <source>Use translation file: {file}</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳ファイルを使用: {file}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="601"/>
         <source>Download translations for {component}/{language}...</source>
-        <translation type="unfinished"></translation>
+        <translation>{component}/{language}の翻訳をダウンロード...</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="610"/>
         <source>Failed to download translation from: {url} -- {text}</source>
-        <translation type="unfinished"></translation>
+        <translation>次から翻訳をダウンロードできませんでした: {url} -- {text}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="611"/>
         <source>Skipped. This component/language was not found on Weblate.</source>
-        <translation type="unfinished"></translation>
+        <translation>スキップされました。このコンポーネント/言語は Weblate で見つかりませんでした。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="635"/>
         <location filename="LanguageTools/LanguageTools.py" line="658"/>
         <source>Translation files folder is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>翻訳ファイルのフォルダが指定されていません。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="638"/>
         <source>lrelease tool path is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>lリリースツールパスが指定されていません。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="676"/>
         <source>No translation (qm) files were found at {location}</source>
-        <translation type="unfinished"></translation>
+        <translation>{location} に翻訳 (qm) ファイルが見つかりませんでした</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="678"/>
         <source>Update successfully completed.
 Installed {count} translation files in {location}.</source>
-        <translation type="unfinished"></translation>
+        <translation>アップデートが正常に完了しました。
+{count} 個の翻訳ファイルが {location} にインストールされました。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="684"/>
         <source>This Slicer version does not support custom viewer fonts.</source>
-        <translation type="unfinished"></translation>
+        <translation>このスライサー バージョンはカスタム ビューア フォントをサポートしていません。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="707"/>
         <source>Installed {count} font files in {location}.</source>
-        <translation type="unfinished"></translation>
+        <translation>{count} 個のフォント ファイルが {location} にインストールされました。</translation>
     </message>
 </context>
 <context>
