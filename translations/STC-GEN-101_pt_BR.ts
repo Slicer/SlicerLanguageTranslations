5b21ee1aa9f3f4802591f2f90ec586e13d0238d7
Luiz Otavio Murta Junior <murta@usp.br>
2026-02-22 18:34:58 +0100

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: 3D Slicer/STC-GEN-101
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-gen-101/pt_BR/

--- translations/STC-GEN-101_pt_BR.ts
+++ translations/STC-GEN-101_pt_BR.ts
@@ -19,7 +19,7 @@
         <location filename="text_dict_default.json" line="12"/>
         <source>28/08/2024</source>
         <extracomment>0_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>28/08/2024</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -34,7 +34,7 @@ Harvard	Medical	School	</source>
         <location filename="text_dict_default.json" line="16"/>
         <source>Goal</source>
         <extracomment>1_Goal_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Objetivo</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
@@ -46,7 +46,7 @@ Harvard	Medical	School	</source>
         <location filename="text_dict_default.json" line="20"/>
         <source>3D Slicer version 4.8</source>
         <extracomment>2_3DSlicerversion48_title</extracomment>
-        <translation type="unfinished">3D Slicer versão 4.8</translation>
+        <translation>3D Slicer versão 4.8</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
@@ -65,14 +65,14 @@ Click	on	the	arrow
 symbol	to	display	the	
 content	of	each	tab.	</source>
         <extracomment>3_TextBox_0</extracomment>
-        <translation type="unfinished">Cada módulo do Slicer
-inclui uma série de abas,
-que dão acesso a
+        <translation>Cada módulo de Slicer
+inclui uma série de guias,
+que dão acesso
 diferentes funcionalidades.
 
-Clique no símbolo de seta
-para exibir o
-conteúdo de cada aba.	</translation>
+Clique na seta
+símbolo para exibir o
+conteúdo de cada guia.	</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
@@ -114,7 +114,7 @@ Módulo	</translation>
         <location filename="text_dict_default.json" line="36"/>
         <source>Data probe</source>
         <extracomment>4_ArrowText_5</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sonda de dados</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
@@ -126,7 +126,7 @@ Módulo	</translation>
 6_WelcomeModule_title
 ----------
 9_WelcomeModule_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Módulo de boas-vindas</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
@@ -134,7 +134,9 @@ Módulo	</translation>
 tab	contains	links	to	the	training	
 compendium	and	documentation  pages	of	3D	Slicer	version	4.8.	</source>
         <extracomment>5_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Documentação e Tutoriais
+tab contém links para o treinamento
+compêndio e páginas de documentação da versão 3D Slicer 4.8.	</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
@@ -146,7 +148,13 @@ also	available
 Click	on		Download	Sample	Data	to	
 access	the	Sample	Data	Module	</source>
         <extracomment>6_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>O painel do módulo de boas-vindas contém
+atalhos para carregar diferentes tipos
+de dados. Uma série de dados de amostra são
+também disponível
+
+Clique em Baixar dados de amostra para
+acessar o Módulo de Dados de Amostra	</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -155,7 +163,7 @@ access	the	Sample	Data	Module	</source>
         <extracomment>7_Sampledata_title
 ----------
 8_Sampledata_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Dados de amostra</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
@@ -164,21 +172,26 @@ contains	links	to	different
 sample	datasets	that	can	be	
 downloaded	into	Slicer.	</source>
         <extracomment>7_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>O módulo de dados da amostra
+contém links para diferentes
+conjuntos de dados de amostra que podem ser
+transferido para Slicer.	</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>Click	on	MRHead to	download	the	
 dataset	in	Slicer.	</source>
         <extracomment>8_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Clique em MRHead para baixar o 
+conjunto de dados no Slicer.	</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>The	MR	scan	of	the	brain	appears	
 in	the	2D	viewers.</source>
         <extracomment>9_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>A imagem de ressonância magnética do cérebro aparece
+nos visualizadores 2D.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="58"/>
@@ -187,14 +200,15 @@ in	the	2D	viewers.</source>
         <extracomment>10_MRBrainSampleDataset_title
 ----------
 11_MRBrainSampleDataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Conjunto de dados de amostra cerebral de RM</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
         <source>Position the mouse on the little pin	
 icon	in the top left corner of the red viewer to display the viewer menu</source>
         <extracomment>10_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Posicione o cursor do mouse sobre o pequeno ícone
+de pino no canto superior esquerdo do visualizador vermelho para exibir o menu do visualizador</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
@@ -202,7 +216,8 @@ icon	in the top left corner of the red viewer to display the viewer menu</source
 viewers,	and	on	the	eye	icon	next	to	it	
 to	display	the	slices	in	the	3D	viewer	);</source>
         <extracomment>11_TextBox_0 12_MRBrainSampleDataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Clique no ícone de link para conectar os três visualizadores 2D
+e no ícone de olho ao lado para exibir as fatias no visualizador 3D;</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="69"/>
@@ -211,14 +226,18 @@ appear	in	the	3D	viewer.
 Go	back	to	the	Welcome	module	
 using	the	green	arrow	in	the	toolbar	);</source>
         <extracomment>12_TextBox_0 13_GoingFurther_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>As fatias axiais, coronais e sagitais
+aparecerão no visualizador 3D.
+Voltar ao módulo Bem-vindo
+usando a seta verde na barra de ferramentas );</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="74"/>
         <source>To	learn	more	about	Slicer	and	
 its	different	functionalities, visit the Slicer4.8 compendium</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Para saber mais sobre o Slicer e suas
+diferentes funcionalidades, visite o compêndio do Slicer 4.8</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
@@ -234,7 +253,11 @@ NIH U54EB005148
 Neuroimaging Analysis Center
 NIH P41EB015902</source>
         <extracomment>14_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Aliança Nacional para Computação de Imagens Médicas
+NIH U54EB005148
+
+Centro de Análise de Neuroimagem
+NIH P41EB015902</translation>
     </message>
 </context>
 </TS>
