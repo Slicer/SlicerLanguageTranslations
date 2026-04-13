ba2ba29168d14e1b7d0fd19f15c0c24f9e41ec5a
N'guessan Kouamé Josué <nguessankouamejosue04@gmail.com>
2026-04-13 14:09:22 +0200

    Translated using Weblate (English (United States))
    Currently translated at 0.0% (0 of 45 strings)
    
    Translation: 3D Slicer/STC-SEG-103
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-seg-103/en_US/

--- translations/STC-SEG-103_en-US.ts
+++ translations/STC-SEG-103_en-US.ts
@@ -7,7 +7,7 @@
         <location filename="text_dict_default.json" line="8"/>
         <source>AI-based Segmentation in 3D Slicer</source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">AI-based Segmentation in 3D Slicer</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
