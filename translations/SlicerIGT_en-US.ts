db685ff7f2d457f84d2a09805d35d0b85e6e3486
N'guessan Kouamé Josué <nguessankouamejosue04@gmail.com>
2026-04-13 14:09:15 +0200

    Translated using Weblate (English (United States))
    Currently translated at 0.0% (0 of 565 strings)
    
    Translation: 3D Slicer/SlicerIGT
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/slicerigt/en_US/

--- translations/SlicerIGT_en-US.ts
+++ translations/SlicerIGT_en-US.ts
@@ -9,7 +9,10 @@
 This module registers fiducial list to a model surface using iterative closest points (ICP) method.
 For help on how to use this module visit: &lt;a href=&apos;https://www.slicerigt.org&apos;&gt;SlicerIGT website&lt;/a&gt;.
     </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">
+This module registers fiducial list to a model surface using iterative closest points (ICP) method.
+For help on how to use this module visit: &lt;a href=&apos;https://www.slicerigt.org&apos;&gt;SlicerIGT website&lt;/a&gt;.
+    </translation>
     </message>
     <message>
         <location filename="FiducialsToModelRegistration/FiducialsToModelRegistration.py" line="24"/>
