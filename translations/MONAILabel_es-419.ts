eaf1f75fbc9bc9b93479f90b9275ac37738e3438
avilchisg <hvigady@gmail.com>
2026-05-04 16:16:09 +0200

    Translated using Weblate (Spanish (Latin America))
    Currently translated at 100.0% (181 of 181 strings)
    
    Translation: 3D Slicer/MONAILabel
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/monailabel/es_419/

--- translations/MONAILabel_es-419.ts
+++ translations/MONAILabel_es-419.ts
@@ -170,12 +170,12 @@ Desarrollado por NVIDIA, KCL
     <message>
         <location filename="MONAILabel/MONAILabel.py" line="197"/>
         <source>Enable this option to include dicom files in server-client data exchange</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar esta opción para incluir archivos DICOM en el intercambio de datos entre servidor y cliente</translation>
     </message>
     <message>
         <location filename="MONAILabel/MONAILabel.py" line="200"/>
         <source>Include DICOM files:</source>
-        <translation type="unfinished"></translation>
+        <translation>Incluir archivos DICOM:</translation>
     </message>
     <message>
         <location filename="MONAILabel/MONAILabel.py" line="215"/>
