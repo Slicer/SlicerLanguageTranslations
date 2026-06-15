aa8a577f36fcb0a8fc436129d1e1c2f1cf7e788e
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-06-15 23:32:34 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: 3D Slicer/STC-GEN-102
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-gen-102/ta/

--- translations/STC-GEN-102_ta.ts
+++ translations/STC-GEN-102_ta.ts
@@ -8,13 +8,14 @@
         <source>Slicer4 Minute
 </source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் 4 மணித்துளி
+</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
         <source>Sonia Pujol, Ph.D.</source>
         <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>சோனியா புசோல், Ph.D.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
@@ -22,7 +23,9 @@
 Brigham and Women’s Hospital
 Harvard Medical School</source>
         <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>கதிரியக்கவியல் உதவிப் பேராசிரியர் 
+பிரிகாம் மற்றும் மகளிர் மருத்துவமனை 
+ஆர்வர்ட் மருத்துவப் பள்ளி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -31,19 +34,19 @@ Harvard Medical School</source>
         <extracomment>1_TextBox_0
 ----------
 16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer4 நிமிட பயிற்சி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>This tutorial is a 4-minute introduction to the 3D visualization capabilities of the Slicer5 software for medical image analysis. </source>
         <extracomment>1_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>இந்த டுடோரியல் மருத்துவப் படப் பகுப்பாய்விற்கான ச்லைசர்5 மென்பொருளின் 3டி காட்சிப்படுத்தல் திறன்களுக்கான 4 நிமிட அறிமுகமாகும். </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
         <source>Slicer5 software &amp; dataset</source>
         <extracomment>2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer5 மென்பொருள் &amp; தரவுத்தொகுப்பு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -51,19 +54,21 @@ Harvard Medical School</source>
 
 *Download the Slicer4minute dataset available at https://www.slicer.org/wiki/Documentation/4.10/Training</source>
         <extracomment>2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>* http://download.slicer.org இல் கிடைக்கும் Slicer5 மென்பொருளைப் பதிவிறக்கவும் 
+
+* https://www.slicer.org/wiki/Documentation/4.10/Training இல் கிடைக்கும் Slicer4minute தரவுத்தொகுப்பைப் பதிவிறக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
         <source>3D Slicer version 5</source>
         <extracomment>3_3DSlicerversion5_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D ச்லைசர் பதிப்பு 5</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>3D Slicer Scene</source>
         <extracomment>4_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D ச்லைசர் காட்சி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
@@ -72,31 +77,34 @@ Harvard Medical School</source>
 *The scene file and datasets have been saved as a MRB (Medical Reality Bundle) file. 
 *The MRB file format is Slicer&apos;s archive file format.</source>
         <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*ஒரு ச்லைசர் காட்சி என்பது MRML (மருத்துவ ரியாலிட்டி மாடலிங் மொழி) கோப்பாகும், இது ச்லைசரில் ஏற்றப்பட்ட உறுப்புகளின் பட்டியலைக் கொண்டுள்ளது (தொகுதிகள், மாதிரிகள், நம்பிக்கைகள், உருமாற்றங்கள் போன்றவை.) 
+*பின்வரும் எடுத்துக்காட்டில், எம்ஆர்ஐ வருடு மற்றும் தலையின் 3டி மாடல்களைக் கொண்ட &apos;Slicer4minute.mrml&apos; காட்சியைப் பயன்படுத்துகிறோம். 
+*காட்சி கோப்பு மற்றும் தரவுத்தொகுப்புகள் MRB (மருத்துவ ரியாலிட்டி பண்டில்) கோப்பாக சேமிக்கப்பட்டுள்ளன. 
+*MRB கோப்பு வடிவம் ச்லைசரின் காப்பக கோப்பு வடிவமாகும்.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
         <source>Loading the Slicer4minute dataset</source>
         <extracomment>5_LoadingtheSlicer4minutedataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer4minute தரவுத்தொகுப்பை ஏற்றுகிறது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
         <source>Drag and drop the slicer4minute.mrb to load the scene in Slicer</source>
         <extracomment>5_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசரில் காட்சியை ஏற்ற slicer4minute.mrb ஐ இழுத்து விடுங்கள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
         <source>Slicer4minute Scene</source>
         <extracomment>6_Slicer4minuteScene_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் 4 நிமிட காட்சி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Slicer displays the elements of the slicer4minute scene. The scene contains and MRI scan and 3D surface models of the brain.</source>
         <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer4minute காட்சியின் கூறுகளை Slicer காட்டுகிறது. காட்சியில் மூளையின் MRI வருடு மற்றும் 3D மேற்பரப்பு மாதிரிகள் உள்ளன.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
@@ -120,26 +128,27 @@ Harvard Medical School</source>
 14_3DVisualization_title
 ----------
 15_3DVisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D காட்சிப்படுத்தல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
         <source>Select the module Models</source>
         <extracomment>7_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>தொகுதி மாதிரிகளைத் தேர்ந்தெடுக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
         <source>3D visualization</source>
         <extracomment>8_3Dvisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D காட்சிப்படுத்தல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="42"/>
         <source>Click on the pin icon on the top left corner of the red slice to display the slice Viewer menu.
 Click on the eye to display the axial slice in the 3D viewer</source>
         <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைச் வியூவர் மெனுவைக் காட்ட, சிவப்பு ச்லைசின் மேல் இடது மூலையில் உள்ள பின் ஐகானைக் சொடுக்கு செய்யவும். 
+3D வியூவரில் அச்சுப் பகுதியைக் காட்ட கண்ணின் மீது சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -147,32 +156,35 @@ Click on the eye to display the axial slice in the 3D viewer</source>
 
 Slicer simultaneously displays the axial slice in the 3D viewer</source>
         <extracomment>9_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>அச்சு MR ச்லைச்கள் மூலம் உலாவ சிவப்பு பார்வையாளரின் ச்லைடரைப் பயன்படுத்தவும். 
+
+ச்லைசர் ஒரே நேரத்தில் 3D வியூவரில் அச்சுப் பகுதியைக் காட்டுகிறது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
         <source>Select the Skin model and lower its opacity using the Opacity slider in the 3D Display tab</source>
         <extracomment>10_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>தேர்ந்தெடு the Skin மாதிரியுரு and lower its opacity using the Opacity slider in the 3D காட்சி தாவல்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>The skull_bone.vtk model appears through the skin.</source>
         <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>skull_bone.vtk மாதிரி தோல் வழியாகத் தோன்றும்.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>Position the mouse in the 3D viewer, click on the left-mouse button to drag and rotate the model. 
 Click on the right-mouse button to zoom in and out</source>
         <extracomment>11_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D வியூவரில் மவுசை வைத்து, மாதிரியை இழுத்து சுழற்ற இடது சுட்டி பொத்தானைக் சொடுக்கு செய்யவும். 
+பெரிதாக்கவும், வெளியேறவும் வலது சுட்டி பொத்தானைக் சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="58"/>
         <source>Anatomical Views</source>
         <extracomment>12_AnatomicalViews_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>உடற்கூறியல் பார்வைகள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
@@ -180,19 +192,21 @@ Click on the right-mouse button to zoom in and out</source>
 
 Click on the eye icon to display the axial and coronal slice in the 3D viewer</source>
         <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைச் வியூவர் மெனுவைக் காட்ட, சிவப்பு மற்றும் பச்சை வியூவரின் மேல் இடது மூலையில் உள்ள பின் ஐகான்களைக் சொடுக்கு செய்யவும் 
+
+3D வியூவரில் அச்சு மற்றும் கரோனல் ச்லைசைக் காட்ட கண் ஐகானைக் சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
         <source>Turn off the visibility of the skull to display the brain white matter model</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூளையின் வெள்ளைப் பொருள் மாதிரியைக் காட்ட, மண்டை ஓட்டின் தெரிவுநிலையை அணைக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="68"/>
         <source>The white matter surface, as well as the left and right optic nerves, appear in the viewer</source>
         <extracomment>14_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>வெள்ளைப் பொருள் மேற்பரப்பு, அதே போல் இடது மற்றும் வலது பார்வை நரம்புகள் பார்வையாளரில் தோன்றும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="72"/>
@@ -202,7 +216,11 @@ Check Clipping in the 3D Display tab
 
 In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; and check &apos;Negative&apos;</source>
         <extracomment>15_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>hemispheric_white_matter.vtk மாதிரியைத் தேர்ந்தெடுக்கவும் 
+
+3D காட்சி தாவலில் கிளிப்பிங்கைச் சரிபார்க்கவும் 
+
+கிளிப்பிங் பிளேன்ச் தாவலில், &apos;கிரீன் ச்லைச் கிளிப்பிங்&apos; என்ற விருப்பத்தைத் தேர்ந்தெடுத்து, &apos;எதிர்மறை&apos; என்பதைச் சரிபார்க்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
@@ -210,13 +228,15 @@ In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; a
 
 *The Slicer5 training compendium contains a series of tutorials and pre-computed datasets to learn how to use the software.</source>
         <extracomment>16_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*இந்த டுடோரியல் MRI தரவு மற்றும் ச்லைசரில் உள்ள 3D மாடல்களின் ஊடாடும் 3D காட்சிப்படுத்தல் பற்றிய ஒரு சிறிய அறிமுகமாகும். 
+
+*Slicer5 பயிற்சித் தொகுப்பானது, மென்பொருளை எவ்வாறு பயன்படுத்துவது என்பதை அறிய, தொடர்ச்சியான பயிற்சிகள் மற்றும் முன்-கணிக்கப்பட்ட தரவுத்தொகுப்புகளைக் கொண்டுள்ளது.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="78"/>
         <source>Acknowledgements</source>
         <extracomment>17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>அங்கீகாரங்கள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="80"/>
@@ -228,7 +248,13 @@ Neuroimage Analysis Center
 NIH P41EB015902
 </source>
         <extracomment>17_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மருத்துவ படத்திற்கான தேசிய கூட்டணி 
+கம்ப்யூட்டிங் 
+NIH U54EB005149 
+
+நியூரோஇமேச் பகுப்பாய்வு நடுவண் 
+NIH P41EB015902
+</translation>
     </message>
 </context>
 </TS>
