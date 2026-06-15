cfb524b41c58b637ff71b73d3619999a83302af5
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-06-15 05:24:02 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (39 of 39 strings)
    
    Translation: 3D Slicer/STC-GEN-101
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-gen-101/ta/

--- translations/STC-GEN-101_ta.ts
+++ translations/STC-GEN-101_ta.ts
@@ -7,13 +7,13 @@
         <location filename="text_dict_default.json" line="8"/>
         <source>Slicer Welcome</source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் வரவேற்கிறோம்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
         <source>Sonia Pujol, Ph.D.</source>
         <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>சோனியா புசோல், Ph.D.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
@@ -24,19 +24,24 @@ Brigham and Women’s Hospital
 Harvard Medical School
 </source>
         <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>கதிரியக்கவியல் உதவிப் பேராசிரியர் 
+
+பிரிகாம் மற்றும் மகளிர் மருத்துவமனை 
+
+ஆர்வர்ட் மருத்துவப் பள்ளி
+</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
         <source>Goal</source>
         <extracomment>1_Goal_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>இலக்கு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>This tutorial is a short introduction to the Welcome module of the Slicer open-source software.</source>
         <extracomment>1_Goal_body</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>இந்த டுடோரியல் ச்லைசர் ஓப்பன் சோர்ச் மென்பொருளின் வரவேற்பு தொகுதிக்கான ஒரு சிறிய அறிமுகமாகும்.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
@@ -45,7 +50,7 @@ Harvard Medical School
         <extracomment>2_TextBox_0
 ----------
 3_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer5 அடிப்படைகள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -53,19 +58,21 @@ Harvard Medical School
 *The platform is developed through a multi-institution effort of several NIH funded large-scale consortia.
 *Slicer is for medical research only, and is not FDA approved. </source>
         <extracomment>2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*Slicer என்பது மருத்துவ இமேசிங் தரவைப் பிரித்தல், பதிவு செய்தல் மற்றும் காட்சிப்படுத்துதல் ஆகியவற்றுக்கான ஒரு திறந்த மூல மென்பொருளாகும். 
+*பல NIH நிதியுதவி பெற்ற பெரிய அளவிலான கூட்டமைப்புகளின் பல நிறுவன முயற்சியின் மூலம் இயங்குதளம் உருவாக்கப்பட்டுள்ளது. 
+*ச்லைசர் மருத்துவ ஆராய்ச்சிக்காக மட்டுமே, FDA அங்கீகரிக்கப்படவில்லை. </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>3D Slicer 5 version 5.10.0 includes over 100 modules and more than 190 extensions for image segmentation, registration and 3D visualization of medical imaging data.</source>
         <extracomment>3_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D ச்லைசர் 5 பதிப்பு 5.10.0 ஆனது 100 க்கும் மேற்பட்ட தொகுதிகள் மற்றும் படப் பிரிவு, பதிவு மற்றும் மருத்துவ இமேசிங் தரவின் 3D காட்சிப்படுத்தல் ஆகியவற்றிற்கான 190 க்கும் மேற்பட்ட நீட்டிப்புகளை உள்ளடக்கியது.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
         <source>Supported Platforms</source>
         <extracomment>4_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ஆதரிக்கப்படும் தளங்கள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
@@ -73,13 +80,15 @@ Harvard Medical School
 
 *Slicer requires a minimum of 2 GB of RAM and a dedicated graphic accelerator with 64 MB of on-board graphic memory. </source>
         <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*Slicer என்பது Mac OSX, Linux மற்றும் சாளரங்கள் இல் உருவாக்கப்பட்டு பராமரிக்கப்படும் பல-தளம் மென்பொருளாகும். 
+
+*ச்லைசருக்கு குறைந்தபட்சம் 2 சிபி ரேம் மற்றும் 64 எம்பி ஆன்-போர்டு கிராஃபிக் மெமரியுடன் கூடிய பிரத்யேக கிராஃபிக் முடுக்கி தேவை. </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
         <source>Welcome to Slicer</source>
         <extracomment>5_WelcometoSlicer_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசருக்கு வரவேற்கிறோம்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
@@ -87,43 +96,45 @@ Harvard Medical School
 
 Click on the arrow symbol to display the content of each tab. </source>
         <extracomment>5_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசரின் ஒவ்வொரு தொகுதியும் ஒரு தொடர் தாவல்களை உள்ளடக்கியது, அவை வெவ்வேறு செயல்பாடுகளை அணுகும். 
+
+ஒவ்வொரு தாவலின் உள்ளடக்கத்தையும் காட்ட அம்புக்குறியின் மீது சொடுக்கு செய்யவும். </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Slicer User Interface</source>
         <extracomment>6_SlicerUserInterface_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் பயனர் இடைமுகம்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
         <source>Toolbar</source>
         <extracomment>6_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>கருவிப்பட்டி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
         <source>3D viewer</source>
         <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D பார்வையாளர்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
         <source>User Interface (UI) panel of the Slicer Welcome Module</source>
         <extracomment>6_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் வரவேற்பு தொகுதியின் பயனர் இடைமுகம் (UI) குழு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="42"/>
         <source>Data probe</source>
         <extracomment>6_TextBox_5</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>தரவு ஆய்வு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
         <source>2D anatomical viewers</source>
         <extracomment>6_ArrowText_6</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>2டி உடற்கூறியல் பார்வையாளர்கள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -135,13 +146,13 @@ Click on the arrow symbol to display the content of each tab. </source>
 8_WelcomeModule_title
 ----------
 12_WelcomeModule_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>வரவேற்பு தொகுதி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
         <source>The Documentation &amp; Tutorials tab contains links to the training compendium and documentation pages of 3D Slicer.</source>
         <extracomment>7_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ஆவணம் &amp; பயிற்சிகள் தாவலில் 3D ச்லைசரின் பயிற்சி தொகுப்பு மற்றும் ஆவணப் பக்கங்களுக்கான இணைப்புகள் உள்ளன.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
@@ -149,7 +160,9 @@ Click on the arrow symbol to display the content of each tab. </source>
 
 Click on Download Sample Data to access the Sample Data Module</source>
         <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>வெல்கம் மாட்யூல் பேனலில் பல்வேறு வகையான தரவை ஏற்றுவதற்கான குறுக்குவழிகள் உள்ளன. தொடர்ச்சியான மாதிரி தரவுகளும் கிடைக்கின்றன. 
+
+மாதிரி தரவு தொகுதியை அணுக பதிவிறக்க மாதிரி தரவை சொடுக்கு செய்யவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="54"/>
@@ -161,57 +174,59 @@ Click on Download Sample Data to access the Sample Data Module</source>
 10_SampleData_title
 ----------
 11_SampleData_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மாதிரி தரவு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>The Sample Data module contains links to different sample datasets that can be downloaded into Slicer.</source>
         <extracomment>9_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மாதிரி தரவு தொகுதியானது ச்லைசரில் பதிவிறக்கம் செய்யக்கூடிய வெவ்வேறு மாதிரி தரவுத்தொகுப்புகளுக்கான இணைப்புகளைக் கொண்டுள்ளது.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
         <source>Brain MR</source>
         <extracomment>10_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூளை எம்.ஆர்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="62"/>
         <source>Chest CT</source>
         <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மார்பு சி.டி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
         <source>Cardiac CT</source>
         <extracomment>10_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>கார்டியாக் சி.டி</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="66"/>
         <source>Diffusion Tensor Imaging (DTI) Dataset</source>
         <extracomment>10_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>டிஃப்யூசன் டென்சர் இமேசிங் (டிடிஐ) தரவுத்தொகுப்பு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="68"/>
         <source>Brain MRI (tumor patient)</source>
         <extracomment>10_TextBox_4</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூளை எம்ஆர்ஐ (கட்டி நோயாளி)</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="72"/>
         <source>Click on MRHead to download the
 dataset in Slicer.</source>
         <extracomment>11_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>பதிவிறக்கம் செய்ய MRHead ஐ சொடுக்கு செய்யவும் 
+ச்லைசரில் தரவுத்தொகுப்பு.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
         <source>The MR scan of the brain appears
 in the 2D viewers.</source>
         <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூளையின் எம்ஆர்ஐ வருடு தோன்றுகிறது 
+2டி வியூவரில்.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="78"/>
@@ -223,27 +238,29 @@ in the 2D viewers.</source>
 14_MRBrainSampleDataset_title
 ----------
 15_MRBrainSampleDataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>எம்ஆர் மூளை மாதிரி தரவுத்தொகுப்பு</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="80"/>
         <source>Position the mouse on the little pin icon in the top left corner of the red viewer to display the viewer menu</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>பார்வையாளர் மெனுவைக் காட்ட, சிவப்பு பார்வையாளரின் மேல் இடது மூலையில் உள்ள சிறிய பின் ஐகானில் சுட்டியை வைக்கவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="84"/>
         <source>Click on the link icon to link all three 2D viewers, and on the eye icon next to it
 to display the slices in the 3D viewer</source>
         <extracomment>14_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மூன்று 2D பார்வையாளர்களையும் இணைக்க இணைப்பு ஐகானையும் அதற்கு அடுத்துள்ள கண் ஐகானையும் சொடுக்கு செய்யவும் 
+துண்டுகளை 3D வியூவரில் காட்ட</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="88"/>
         <source>The axial, coronal and sagittal slices appear in the 3D viewer.
 Go back to the Welcome module using the green arrow in the toolbar</source>
         <extracomment>15_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>அச்சு, கொரோனல் மற்றும் சாகிட்டல் துண்டுகள் 3D வியூவரில் தோன்றும். 
+கருவிப்பட்டியில் பச்சை அம்புக்குறியைப் பயன்படுத்தி வரவேற்பு தொகுதிக்குச் செல்லவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="90"/>
@@ -252,25 +269,25 @@ Go back to the Welcome module using the green arrow in the toolbar</source>
         <extracomment>16_GoingFurther_title
 ----------
 17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மேலும் செல்கிறது</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="92"/>
         <source>To learn more about Slicer and its different functionalities, visit the Slicer Compendium</source>
         <extracomment>16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ச்லைசர் மற்றும் அதன் பல்வேறு செயல்பாடுகளைப் பற்றி மேலும் அறிய, ச்லைசர் தொகுப்பைப் பார்வையிடவும்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="96"/>
         <source>https://training.slicer.org/</source>
         <extracomment>17_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>https://training.slicer.org/</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="98"/>
         <source>Acknowledgements</source>
         <extracomment>18_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>அங்கீகாரங்கள்</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="100"/>
@@ -283,7 +300,14 @@ NIH P41EB015902
 
 Chan Zuckerberg Initiative (CZI)</source>
         <extracomment>18_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>மருத்துவ படத்திற்கான தேசிய கூட்டணி 
+கம்ப்யூட்டிங் 
+NIH U54EB005149 
+
+நியூரோஇமேச் பகுப்பாய்வு நடுவண் 
+NIH P41EB015902 
+
+சான் சுக்கர்பெர்க் முன்முயற்சி (CZI)</translation>
     </message>
 </context>
 </TS>
