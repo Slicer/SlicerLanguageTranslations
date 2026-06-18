d72ece499b2efdc663c57d77a9dd31b523b8eb57
lassoan <307929+lassoan@users.noreply.github.com>
2026-06-18 13:26:17 +0000

    Automatic translations using DeepL

--- translations/STC-GEN-101_zh_Hans.ts
+++ translations/STC-GEN-101_zh_Hans.ts
@@ -7,13 +7,13 @@
         <location filename="text_dict_default.json" line="8"/>
         <source>Slicer Welcome</source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer 欢迎</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
         <source>Sonia Pujol, Ph.D.</source>
         <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">索尼娅·普约尔，博士</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
@@ -24,19 +24,24 @@ Brigham and Women’s Hospital
 Harvard Medical School
 </source>
         <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">放射科助理教授
+
+布里格姆妇女医院
+
+哈佛医学院
+</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
         <source>Goal</source>
         <extracomment>1_Goal_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">目标</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>This tutorial is a short introduction to the Welcome module of the Slicer open-source software.</source>
         <extracomment>1_Goal_body</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">本教程简要介绍了开源软件 Slicer 的“欢迎”模块。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
@@ -45,7 +50,7 @@ Harvard Medical School
         <extracomment>2_TextBox_0
 ----------
 3_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer5 基础知识</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -53,19 +58,21 @@ Harvard Medical School
 *The platform is developed through a multi-institution effort of several NIH funded large-scale consortia.
 *Slicer is for medical research only, and is not FDA approved. </source>
         <extracomment>2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">*Slicer 是一款用于医学影像数据分割、配准和可视化的开源软件。
+*该平台是由多个由美国国立卫生研究院（NIH）资助的大型联盟通过多机构合作开发的。
+*Slicer 仅用于医学研究，尚未获得美国食品药品监督管理局（FDA）的批准。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>3D Slicer 5 version 5.10.0 includes over 100 modules and more than 190 extensions for image segmentation, registration and 3D visualization of medical imaging data.</source>
         <extracomment>3_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D Slicer 5 5.10.0 版本包含 100 多个模块和 190 多个扩展，用于医学影像数据的图像分割、配准和 3D 可视化。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
         <source>Supported Platforms</source>
         <extracomment>4_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">支持的平台</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
@@ -73,13 +80,15 @@ Harvard Medical School
 
 *Slicer requires a minimum of 2 GB of RAM and a dedicated graphic accelerator with 64 MB of on-board graphic memory. </source>
         <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">*Slicer 是一款在 Mac OS X、Linux 和 Windows 平台上开发和维护的多平台软件。
+
+*Slicer 至少需要 2 GB 内存，以及一块配备 64 MB 板载显存的独立显卡。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
         <source>Welcome to Slicer</source>
         <extracomment>5_WelcometoSlicer_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">欢迎使用 Slicer</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
@@ -87,43 +96,45 @@ Harvard Medical School
 
 Click on the arrow symbol to display the content of each tab. </source>
         <extracomment>5_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer 的每个模块都包含一系列选项卡，可通过这些选项卡访问不同的功能。
+
+点击箭头图标即可显示每个选项卡的内容。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Slicer User Interface</source>
         <extracomment>6_SlicerUserInterface_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer 用户界面</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
         <source>Toolbar</source>
         <extracomment>6_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">工具栏</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
         <source>3D viewer</source>
         <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D 查看器</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
         <source>User Interface (UI) panel of the Slicer Welcome Module</source>
         <extracomment>6_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer 欢迎模块的用户界面（UI）面板</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="42"/>
         <source>Data probe</source>
         <extracomment>6_TextBox_5</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">样本日期</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="44"/>
         <source>2D anatomical viewers</source>
         <extracomment>6_ArrowText_6</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">2D解剖图像查看器</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -135,13 +146,13 @@ Click on the arrow symbol to display the content of each tab. </source>
 8_WelcomeModule_title
 ----------
 12_WelcomeModule_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">欢迎模块</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="48"/>
         <source>The Documentation &amp; Tutorials tab contains links to the training compendium and documentation pages of 3D Slicer.</source>
         <extracomment>7_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">“文档与教程”选项卡中包含指向 3D Slicer 培训手册和文档页面的链接。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
@@ -149,7 +160,9 @@ Click on the arrow symbol to display the content of each tab. </source>
 
 Click on Download Sample Data to access the Sample Data Module</source>
         <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">“欢迎”模块面板中包含用于加载不同类型数据的快捷方式。此外，还提供了一系列示例数据。
+
+点击“下载示例数据”即可访问“示例数据”模块</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="54"/>
@@ -161,57 +174,59 @@ Click on Download Sample Data to access the Sample Data Module</source>
 10_SampleData_title
 ----------
 11_SampleData_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">示例数据</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>The Sample Data module contains links to different sample datasets that can be downloaded into Slicer.</source>
         <extracomment>9_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">“示例数据”模块包含指向不同示例数据集的链接，这些数据集可下载到 Slicer 中。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
         <source>Brain MR</source>
         <extracomment>10_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">脑部磁共振成像</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="62"/>
         <source>Chest CT</source>
         <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">这次CT</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
         <source>Cardiac CT</source>
         <extracomment>10_TextBox_2</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">心脏CT</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="66"/>
         <source>Diffusion Tensor Imaging (DTI) Dataset</source>
         <extracomment>10_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">扩散张量成像（DTI）数据集</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="68"/>
         <source>Brain MRI (tumor patient)</source>
         <extracomment>10_TextBox_4</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">脑部MRI（肿瘤患者）</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="72"/>
         <source>Click on MRHead to download the
 dataset in Slicer.</source>
         <extracomment>11_ArrowText_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">点击 MRHead 下载
+Slicer 中的数据集。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
         <source>The MR scan of the brain appears
 in the 2D viewers.</source>
         <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">脑部MR扫描图像显示在
+2D查看器中。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="78"/>
@@ -223,27 +238,29 @@ in the 2D viewers.</source>
 14_MRBrainSampleDataset_title
 ----------
 15_MRBrainSampleDataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">MR脑组织样本数据集</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="80"/>
         <source>Position the mouse on the little pin icon in the top left corner of the red viewer to display the viewer menu</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">将鼠标悬停在红色查看器左上角的小图钉图标上，即可显示查看器菜单</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="84"/>
         <source>Click on the link icon to link all three 2D viewers, and on the eye icon next to it
 to display the slices in the 3D viewer</source>
         <extracomment>14_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">点击链接图标可将这三个 2D 查看器关联起来，再点击其旁边的眼睛图标
+即可在 3D 查看器中显示切片</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="88"/>
         <source>The axial, coronal and sagittal slices appear in the 3D viewer.
 Go back to the Welcome module using the green arrow in the toolbar</source>
         <extracomment>15_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">轴向、冠状面和矢状面切片将在3D查看器中显示。
+使用工具栏中的绿色箭头返回“欢迎”模块</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="90"/>
@@ -252,25 +269,25 @@ Go back to the Welcome module using the green arrow in the toolbar</source>
         <extracomment>16_GoingFurther_title
 ----------
 17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">更进一步</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="92"/>
         <source>To learn more about Slicer and its different functionalities, visit the Slicer Compendium</source>
         <extracomment>16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">如需进一步了解 Slicer 及其各项功能，请访问《Slicer 指南》</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="96"/>
         <source>https://training.slicer.org/</source>
         <extracomment>17_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">https://training.slicer.org/</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="98"/>
         <source>Acknowledgements</source>
         <extracomment>18_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">致谢</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="100"/>
@@ -283,7 +300,14 @@ NIH P41EB015902
 
 Chan Zuckerberg Initiative (CZI)</source>
         <extracomment>18_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">全国医学影像
+计算联盟
+NIH U54EB005149
+
+神经影像分析中心
+NIH P41EB015902
+
+陈-扎克伯格倡议（CZI）</translation>
     </message>
 </context>
 </TS>
