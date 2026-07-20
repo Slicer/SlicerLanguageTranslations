a1f457b591e1b3504e6494f5f246d1b4633292dd
Lee Vincent <vincentlee986@gmail.com>
2026-07-20 20:22:21 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (61 of 61 strings)
    
    Translation: 3D Slicer/LanguagePacks
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/languagepacks/zh_Hant/

--- translations/LanguagePacks_zh-Hant.ts
+++ translations/LanguagePacks_zh-Hant.ts
@@ -16,22 +16,22 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="96"/>
         <source>Weblate API key:</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate API 金鑰：</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="103"/>
         <source>Leave empty to use deafult. Optional API key for Weblate. Provide this if you need to access private projects or avoid rate limits.</source>
-        <translation type="unfinished"></translation>
+        <translation>留空以使用預設值。Weblate API 金鑰為選填；如需存取私人專案或避免速率限制，請提供此金鑰。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="123"/>
         <source>Leave empty to use default (Qt lrelease tools that is bundled with the Slicer installer or available in system paths). Specify to use custom Qt lrelease executable.</source>
-        <translation type="unfinished"></translation>
+        <translation>留空以使用預設值 (Slicer 安裝程式內附或可從系統路徑使用的 Qt lrelease 工具)。如要使用自訂 Qt lrelease 執行檔，請指定其路徑。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="136"/>
         <source>Text Direction:</source>
-        <translation type="unfinished"></translation>
+        <translation>文字方向：</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="210"/>
@@ -56,7 +56,7 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="246"/>
         <source>GitHub (updated daily)</source>
-        <translation>GitHub (每天更新)</translation>
+        <translation>GitHub (每日更新)</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="253"/>
@@ -66,7 +66,7 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="260"/>
         <source>Latest file only:</source>
-        <translation>僅最新檔案：</translation>
+        <translation>僅使用最新檔案：</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="270"/>
@@ -81,17 +81,17 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="280"/>
         <source>Select language(s) that will be downloaded and installed.</source>
-        <translation>選擇欲下載並安裝的語言。</translation>
+        <translation>選取要下載及安裝的語言。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="287"/>
         <source>Folder containing translation (.ts) files.</source>
-        <translation>資料夾內有翻譯檔 (.ts) 。</translation>
+        <translation>包含翻譯檔 (.ts) 的資料夾。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="302"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If this option is checked then only the latest .ts file in the folder will be used. Enable this option to ignore previously downloaded .ts files are saved in the same folder.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;若勾選此選項，則只會使用資料夾中最新的 .ts 檔案。啟用此選項忽略同資料夾內之前下載的所有 .ts 檔案。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;若勾選此選項，則僅使用資料夾中最新的 .ts 檔案。若同一資料夾中儲存了先前下載的 .ts 檔案，請啟用此選項加以忽略。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="168"/>
@@ -102,17 +102,17 @@
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="143"/>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="181"/>
         <source>Change only takes effect after restarting the application.</source>
-        <translation>變更在重啟程式之後才會生效。</translation>
+        <translation>變更僅會在重新啟動應用程式後生效。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="191"/>
         <source>Restart the application to see updated translations on the graphical user interface.</source>
-        <translation>重啟程式以在使用者圖形介面顯示更新後的翻譯。</translation>
+        <translation>重新啟動應用程式，即可在圖形使用者介面中查看更新後的翻譯。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="194"/>
         <source>Restart the application</source>
-        <translation>重啟程式</translation>
+        <translation>重新啟動應用程式</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="27"/>
@@ -128,7 +128,7 @@
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="46"/>
         <source>List of folders where the application looks for translations. This is for information only, the paths cannot be changed.</source>
         <oldsource>Path to Qt lrelease executable. Leave empty to use default (the one bundled with the Slicer installer or available in system paths).</oldsource>
-        <translation type="unfinished">Qt lrelease 執行檔路徑。留空以使用預設值 (附帶於 Slicer 安裝程式或系統路徑中可用的執行檔)。</translation>
+        <translation>應用程式搜尋翻譯檔的資料夾清單。此資訊僅供參考，路徑無法變更。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="62"/>
@@ -138,7 +138,7 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="69"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;master, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;Only used for GitHub.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;master, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;僅用於GitHub。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;master, 4.13, 4.14, ...&lt;/p&gt;&lt;p&gt;僅用於 GitHub。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="79"/>
@@ -148,7 +148,7 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="86"/>
         <source>Github repository that stores the translated strings.</source>
-        <translation>儲存已翻譯字串的 Github 儲存庫。</translation>
+        <translation>儲存翻譯字串的 GitHub 儲存庫。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="39"/>
@@ -158,7 +158,7 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="55"/>
         <source>Show more...</source>
-        <translation>顯示更多...</translation>
+        <translation>顯示更多……</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="318"/>
@@ -173,12 +173,12 @@
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="339"/>
         <source>If enabled, then the shortcut opens the text finder, which can be used to extract text from the application user interface and open it in the translation website.</source>
-        <translation>啟用後，可使用快捷鍵開啟文字搜尋器，用來從程式使用者介面抽取文字並在翻譯網站中開啟。</translation>
+        <translation>啟用後，快捷鍵會開啟文字搜尋器，可用來從應用程式使用者介面擷取文字，並在翻譯網站上開啟該文字。</translation>
     </message>
     <message>
         <location filename="LanguageTools/Resources/UI/LanguageTools.ui" line="364"/>
         <source>Keyboard shortcut:</source>
-        <translation>快捷鍵:</translation>
+        <translation>鍵盤快捷鍵：</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="21"/>
@@ -191,7 +191,10 @@
 This module can build translation files and install them locally. It is useful for creating and testing translations.
 See more information in the &lt;a href=&quot;https://github.com/SoniaPujolLab/SlicerLanguagePacks&quot;&gt;extension&apos;s documentation&lt;/a&gt;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+此模組可建置翻譯檔並安裝至本機，適合用於建立及測試翻譯。
+如需詳細資訊，請參閱 &lt;a href=&quot;https://github.com/SoniaPujolLab/SlicerLanguagePacks&quot;&gt;擴充功能文件&lt;/a&gt;。
+</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="29"/>
@@ -202,15 +205,14 @@ Developed of this module was partially funded by &lt;a href=&quot;https://chanzu
 This module can build translation files and install them locally. It is useful for creating and testing translations.
 See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt;extension&apos;s documentation&lt;/a&gt;.
 </oldsource>
-        <translation type="unfinished">
-此模組可建置並於本機安裝翻譯檔。建立和測試翻譯時非常好用。
-請見 &lt;a href=&quot;https://github.com/Slicer/SlicerLanguagePacks&quot;&gt;附加元件的說明文件&lt;/a&gt;以獲得更多資訊。
+        <translation>
+本模組的部分開發工作由 &lt;a href=&quot;https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-language-internationalization-and-usability-improvements/&quot;&gt;CZI EOSS grant&lt;/a&gt; 資助。
 </translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="178"/>
         <source>Failed to extract any text from widget</source>
-        <translation>從精靈抽取文字失敗</translation>
+        <translation>無法從小工具擷取任何文字</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="193"/>
@@ -220,34 +222,34 @@ See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLan
         <oldsource>Click on the text to find it on the translation website:
 
 {links}</oldsource>
-        <translation>點擊文字以在翻譯網站搜尋 [{language}]：
+        <translation>按一下文字，即可在翻譯網站上尋找 [{language}]：
 
 {links}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="211"/>
         <source>Failed to extract any text from: {object}</source>
-        <translation>從{object}抽取文字失敗</translation>
+        <translation>無法從 {object} 擷取任何文字</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="267"/>
         <source>Auto-detect</source>
-        <translation type="unfinished"></translation>
+        <translation>自動偵測</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="268"/>
         <source>Left to Right</source>
-        <translation type="unfinished"></translation>
+        <translation>由左至右</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="269"/>
         <source>Right to Left</source>
-        <translation type="unfinished"></translation>
+        <translation>由右至左</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="332"/>
         <source>Failed to retrieve language list from Weblate.</source>
-        <translation>從 Weblate 獲取語言清單失敗。</translation>
+        <translation>無法從 Weblate 取得語言清單。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="461"/>
@@ -257,42 +259,42 @@ See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLan
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="483"/>
         <source>Update completed! Select application language and restart the application to see the results.</source>
-        <translation>更新完成！選擇語言並重啟程式以查看結果。</translation>
+        <translation>更新完成！請選取應用程式語言並重新啟動應用程式，以查看結果。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="570"/>
         <source>Failed to query list of languages from Weblate ({status_code}:{reason})</source>
-        <translation>從 Weblate 搜尋語言清單失敗 ({status_code}:{reason})</translation>
+        <translation>無法從 Weblate 查詢語言清單 ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="603"/>
         <source>Failed to query list of components from Weblate ({status_code}:{reason})</source>
-        <translation>從 Weblate 搜尋元件清單失敗 ({status_code}:{reason})</translation>
+        <translation>無法從 Weblate 查詢元件清單 ({status_code}:{reason})</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="642"/>
         <source>No .ts files were found in the specified location.</source>
-        <translation>在指定的位置找不到 .ts 檔。</translation>
+        <translation>在指定位置找不到任何 .ts 檔案。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="646"/>
         <source>Use translation file: {file}</source>
-        <translation>使用翻譯檔案： {file}</translation>
+        <translation>使用翻譯檔：{file}</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="670"/>
         <source>Download translations for {component}/{language}...</source>
-        <translation>下載 {component}/{language} 的翻譯...</translation>
+        <translation>正在下載 {component}/{language} 的翻譯……</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="682"/>
         <source>Skipped. This component/language was not found on Weblate.</source>
-        <translation>已略過。於 Weblate 找不到此元件/語言。</translation>
+        <translation>已略過。Weblate 上找不到此元件／語言組合。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="691"/>
         <source>Skipped. The downloaded file is invalid.</source>
-        <translation type="unfinished"></translation>
+        <translation>已略過。下載的檔案無效。</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="717"/>
@@ -308,7 +310,7 @@ See more information in the &lt;a href=&quot;https://github.com/Slicer/SlicerLan
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="758"/>
         <source>No translation (qm) files were found at {location}</source>
-        <translation>{location}中找不到翻譯檔 (qm)</translation>
+        <translation>{location} 中找不到任何翻譯檔 (qm)</translation>
     </message>
     <message>
         <location filename="LanguageTools/LanguageTools.py" line="760"/>
