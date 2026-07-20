08aa0199f40b4ae511dc06e70a24cfdadb3a3907
Lee Vincent <vincentlee986@gmail.com>
2026-07-20 20:14:16 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: 3D Slicer/STC-GEN-102
    Translate-URL: https://hosted.weblate.org/projects/3d-slicer/stc-gen-102/zh_Hant/

--- translations/STC-GEN-102_zh_Hant.ts
+++ translations/STC-GEN-102_zh_Hant.ts
@@ -8,13 +8,14 @@
         <source>Slicer4 Minute
 </source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer 四分鐘教學
+</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
         <source>Sonia Pujol, Ph.D.</source>
         <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sonia Pujol 博士</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
@@ -22,7 +23,9 @@
 Brigham and Women’s Hospital
 Harvard Medical School</source>
         <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>放射學助理教授
+布萊根婦女醫院
+哈佛醫學院</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -31,19 +34,19 @@ Harvard Medical School</source>
         <extracomment>1_TextBox_0
 ----------
 16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer 四分鐘教學</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>This tutorial is a 4-minute introduction to the 3D visualization capabilities of the Slicer5 software for medical image analysis. </source>
         <extracomment>1_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>本教學將在 4 分鐘內簡介 Slicer 5 醫學影像分析軟體的 3D 視覺化功能。 </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
         <source>Slicer5 software &amp; dataset</source>
         <extracomment>2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer 5 軟體與資料集 (&amp;D)</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -51,19 +54,21 @@ Harvard Medical School</source>
 
 *Download the Slicer4minute dataset available at https://www.slicer.org/wiki/Documentation/4.10/Training</source>
         <extracomment>2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*從 http://download.slicer.org 下載 Slicer 5 軟體
+
+*從 https://www.slicer.org/wiki/Documentation/4.10/Training 下載 Slicer4minute 資料集</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
         <source>3D Slicer version 5</source>
         <extracomment>3_3DSlicerversion5_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D Slicer 版本 5</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>3D Slicer Scene</source>
         <extracomment>4_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D Slicer 場景</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
@@ -72,31 +77,34 @@ Harvard Medical School</source>
 *The scene file and datasets have been saved as a MRB (Medical Reality Bundle) file. 
 *The MRB file format is Slicer&apos;s archive file format.</source>
         <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*Slicer 場景是一個 MRML (Medical Reality Modeling Language) 檔案，其中包含載入 Slicer 的元素清單 (影像體積、模型、基準點、變換等)。
+*在以下範例中，我們使用由頭部 MRI 掃描影像及 3D 模型組成的「Slicer4minute.mrml」場景。
+*場景檔案及資料集已儲存為 MRB (Medical Reality Bundle) 檔案。
+*MRB 檔案格式是 Slicer 的封存檔案格式。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
         <source>Loading the Slicer4minute dataset</source>
         <extracomment>5_LoadingtheSlicer4minutedataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>載入 Slicer4minute 資料集</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
         <source>Drag and drop the slicer4minute.mrb to load the scene in Slicer</source>
         <extracomment>5_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>將 slicer4minute.mrb 拖放至 Slicer，以載入場景</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
         <source>Slicer4minute Scene</source>
         <extracomment>6_Slicer4minuteScene_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer4minute 場景</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Slicer displays the elements of the slicer4minute scene. The scene contains and MRI scan and 3D surface models of the brain.</source>
         <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slicer 會顯示 slicer4minute 場景中的元素。此場景包含腦部 MRI 掃描影像及 3D 表面模型。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
@@ -120,26 +128,27 @@ Harvard Medical School</source>
 14_3DVisualization_title
 ----------
 15_3DVisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D 視覺化</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
         <source>Select the module Models</source>
         <extracomment>7_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>選取「模型」模組</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
         <source>3D visualization</source>
         <extracomment>8_3Dvisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>3D 視覺化</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="42"/>
         <source>Click on the pin icon on the top left corner of the red slice to display the slice Viewer menu.
 Click on the eye to display the axial slice in the 3D viewer</source>
         <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>按一下紅色切片左上角的圖釘圖示，以顯示切片檢視器選單。
+按一下眼睛圖示，以在 3D 檢視器中顯示軸向切片</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -147,32 +156,35 @@ Click on the eye to display the axial slice in the 3D viewer</source>
 
 Slicer simultaneously displays the axial slice in the 3D viewer</source>
         <extracomment>9_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>使用紅色檢視器的滑桿瀏覽軸向 MR 切片。
+
+Slicer 會同時在 3D 檢視器中顯示軸向切片</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
         <source>Select the Skin model and lower its opacity using the Opacity slider in the 3D Display tab</source>
         <extracomment>10_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>選取 Skin 模型，並使用「3D 顯示」分頁中的「不透明度」滑桿降低其不透明度</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>The skull_bone.vtk model appears through the skin.</source>
         <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>可透過 Skin 模型看到 skull_bone.vtk 模型。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>Position the mouse in the 3D viewer, click on the left-mouse button to drag and rotate the model. 
 Click on the right-mouse button to zoom in and out</source>
         <extracomment>11_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>將滑鼠移至 3D 檢視器中，按住滑鼠左鍵拖曳並旋轉模型。
+按下滑鼠右鍵即可放大或縮小</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="58"/>
         <source>Anatomical Views</source>
         <extracomment>12_AnatomicalViews_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>解剖視圖</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
@@ -180,19 +192,21 @@ Click on the right-mouse button to zoom in and out</source>
 
 Click on the eye icon to display the axial and coronal slice in the 3D viewer</source>
         <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>按一下紅色及綠色檢視器左上角的圖釘圖示，以顯示切片檢視器選單
+
+按一下眼睛圖示，以在 3D 檢視器中顯示軸向及冠狀切片</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
         <source>Turn off the visibility of the skull to display the brain white matter model</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>關閉顱骨的可見性，以顯示腦部白質模型</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="68"/>
         <source>The white matter surface, as well as the left and right optic nerves, appear in the viewer</source>
         <extracomment>14_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>白質表面以及左、右視神經會顯示在檢視器中</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="72"/>
@@ -202,7 +216,11 @@ Check Clipping in the 3D Display tab
 
 In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; and check &apos;Negative&apos;</source>
         <extracomment>15_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>選取 hemispheric_white_matter.vtk 模型
+
+在「3D 顯示」分頁中核取「裁切」
+
+在「裁切平面」分頁中選取「綠色切片裁切」選項，並核取「負側」</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
@@ -210,13 +228,15 @@ In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; a
 
 *The Slicer5 training compendium contains a series of tutorials and pre-computed datasets to learn how to use the software.</source>
         <extracomment>16_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>*本教學簡短介紹了如何在 Slicer 中以互動方式對 MRI 資料及 3D 模型進行 3D 視覺化。
+
+*Slicer 5 訓練教材彙編包含一系列教學及預先計算的資料集，可用來學習如何使用此軟體。</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="78"/>
         <source>Acknowledgements</source>
         <extracomment>17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>致謝</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="80"/>
@@ -228,7 +248,13 @@ Neuroimage Analysis Center
 NIH P41EB015902
 </source>
         <extracomment>17_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>國家醫學影像計算
+聯盟
+NIH U54EB005149
+
+神經影像分析中心
+NIH P41EB015902
+</translation>
     </message>
 </context>
 </TS>
