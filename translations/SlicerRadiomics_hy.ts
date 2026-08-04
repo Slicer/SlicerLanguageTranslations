008ada603ba1acb8332a90059075325b5a48dcb8
lassoan <307929+lassoan@users.noreply.github.com>
2026-08-04 05:28:17 +0000

    Automatic translations using DeepL

--- translations/SlicerRadiomics_hy.ts
+++ translations/SlicerRadiomics_hy.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="29"/>
         <source>Radiomics</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ռադիոմիկա</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="38"/>
@@ -15,194 +15,200 @@
     It gives access to the radiomics feature calculation classes implemented in pyradiomics library.
     See more details at http://pyradiomics.readthedocs.io/.
     </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">
+    Սա SlicerRadomics ընդլայնման մեջ ընդգրկված, սցենարավորվող և բեռնվող մոդուլ է։
+    Այն հնարավորություն է տալիս մուտք ունենալ pyradiomics գրադարանում իրականացված ռադիոմիկայի հատկանիշների հաշվարկման դասերին։
+    Մանրամասների համար այցելեք http://pyradiomics.readthedocs.io/։
+    </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="48"/>
         <source>
     This work was partially supported by NIH/NCI ITCR program grant U24 CA194354.
     </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">
+    Այս աշխատանքը մասամբ ֆինանսավորվել է NIH/NCI ITCR ծրագրի U24 CA194354 դրամաշնորհով։
+    </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="101"/>
         <source>Run the algorithm.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Գործարկեք ալգորիթմը։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="132"/>
         <source>Select Input Volume and Segmentation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք մուտքային ծավալը և հատվածավորումը</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="149"/>
         <source>Pick the input image for the feature calculation.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք հատկության հաշվարկման համար մուտքային պատկերը։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="150"/>
         <source>Input Image Volume: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Մուտքային պատկերի ծավալը: </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="165"/>
         <source>Pick the regions for feature calculation - defined by a segmentation or labelmap volume node.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք հատկանիշների հաշվարկման տարածաշրջանները՝ սահմանված հատվածավորման կամ պիտակային քարտեզի ծավալային հանգույցով։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="166"/>
         <source>Input regions: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Մուտքային շրջաններ: </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="171"/>
         <source>Extraction Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Հանման հարմարեցում</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="182"/>
         <source>Manual Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ձեռքով հարմարեցում</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="187"/>
         <source>Parameter File Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Պարամետրային ֆայլի հարմարեցում</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="203"/>
         <source>Feature Classes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Առանձնահատկության դասեր</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="209"/>
         <source>Features:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Հատկություններ:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="232"/>
         <source>Toggle Features:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ֆունկցիաների անջատիչ:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="234"/>
         <source>All Features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Բոլոր հատկությունները</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="236"/>
         <source>Calculate all feature classes.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Հաշվարկեք բոլոր հատկանիշային դասերը։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="238"/>
         <source>No Features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Չկան առանձնահատկություններ</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="240"/>
         <source>Calculate no feature classes.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Չհաշվարկել որևէ հատկանիշային դաս։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="246"/>
         <source>Resampling and Filtering</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Վերանմուշավորում և ֆիլտրում</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="255"/>
         <source>Three floating-point numbers separated by comma defining the resampled pixel size (mm).</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Եռ կետային թվեր, բաժանված ստորակետով, որոնք սահմանում են կրկնօրինակված պիքսելի չափը (մմ)։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="255"/>
         <source>Resampled voxel size</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Նորից նմուշավորված վոքսելի չափ</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="261"/>
         <source>Laplacian of Gaussian filter kernel sizes (mm), separated by comma. If empty, no LoG filtering will be applied.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Գաուսյան ֆիլտրի միջուկի չափերը (մմ), բաժանված ստորակետով։ Եթե դատարկ է, LoG ֆիլտրում չի կիրառվի։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="261"/>
         <source>LoG kernel sizes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">LoG միջուկների չափեր</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="268"/>
         <source>If checked, PyRadiomics will calculate features on the image after applying wavelet transformation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Եթե նշված լինի, PyRadiomics-ը կհաշվարկի առանձնահատկությունները պատկերի վրա՝ ալիքային փոխարկում կիրառելուց հետո։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="268"/>
         <source>Wavelet-based features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Փոքր ալիքների վրա հիմնված հատկանիշներ</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="275"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Կարգավորումներ</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="289"/>
         <source>Set the bin width</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Տեղադրեք զամբյուղի լայնությունը</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="289"/>
         <source>Bin Width</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Թափոնաման լայնություն</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="295"/>
         <source>Use a symmetrical GLCM matrix</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Օգտագործեք սիմետրիկ GLCM մատրիցա</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="295"/>
         <source>Enforce Symmetrical GLCM</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Կիրառել սիմետրիկ GLCM</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="310"/>
         <source>Parameter File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Պարամետրերի ֆայլ</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="315"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ելք</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="324"/>
         <source>If checked, PyRadiomics outputs log messages from level DEBUG and higher (instead of INFO and higher)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Եթե նշված է, PyRadiomics-ը ելք է տալիս DEBUG մակարդակի և ավելի բարձր լոգ հաղորդագրություններ (INFO մակարդակի և ավելի բարձր փոխարեն)</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="324"/>
         <source>Verbose Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Մանրամասն ելք</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="337"/>
         <source>Select the table where features will be saved, resets feature values on each run.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք այն աղյուսակը, որտեղ հատկությունները կպահպանվեն, և որը յուրաքանչյուր գործարկման ժամանակ վերականգնում է հատկությունների արժեքները։</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="337"/>
         <source>Output table:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Արդյունքի աղյուսակ:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="386"/>
         <source>Working...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Աշխատում է...</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="472"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Դիմել</translation>
     </message>
 </context>
 <context>
@@ -210,7 +216,7 @@
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="30"/>
         <source>Informatics</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Տեղեկատվական տեխնոլոգիաներ</translation>
     </message>
 </context>
 </TS>
