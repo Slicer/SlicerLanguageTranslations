0ffd80da6024227d5cd9759394108401f2ccfbaa
lassoan <307929+lassoan@users.noreply.github.com>
2026-08-04 05:26:28 +0000

    Automatic translations using DeepL

--- translations/STC-GEN-102_hy.ts
+++ translations/STC-GEN-102_hy.ts
@@ -8,13 +8,14 @@
         <source>Slicer4 Minute
 </source>
         <extracomment>0_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer4 րոպե
+</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="10"/>
         <source>Sonia Pujol, Ph.D.</source>
         <extracomment>0_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Սոնյա Պուժոլ, փ.դ.</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="12"/>
@@ -22,7 +23,9 @@
 Brigham and Women’s Hospital
 Harvard Medical School</source>
         <extracomment>0_TextBox_3</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ռադիոլոգիայի ասիստենտ պրոֆեսոր
+Բրիգհեմի և կանանց հիվանդանոց
+Հարվարդի բժշկական դպրոց</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="14"/>
@@ -31,19 +34,19 @@ Harvard Medical School</source>
         <extracomment>1_TextBox_0
 ----------
 16_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer. 4 րոպե տևողությամբ ուսուցողական տեսանյութ</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="16"/>
         <source>This tutorial is a 4-minute introduction to the 3D visualization capabilities of the Slicer5 software for medical image analysis. </source>
         <extracomment>1_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Այս ուսուցողական ձեռնարկը 4 րոպե տևողությամբ ներածություն է բժշկական պատկերների վերլուծության համար նախատեսված Slicer5 ծրագրի 3D վիզուալիզացման հնարավորություններին։ </translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="18"/>
         <source>Slicer5 software &amp; dataset</source>
         <extracomment>2_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer5 ծրագրաշար և տվյալների հավաքածու</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="20"/>
@@ -51,19 +54,21 @@ Harvard Medical School</source>
 
 *Download the Slicer4minute dataset available at https://www.slicer.org/wiki/Documentation/4.10/Training</source>
         <extracomment>2_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">*Ներբեռնել Slicer5 ծրագրաշարը՝ հասանելի http://download.slicer.org կայքում
+
+*Ներբեռնել Slicer4minute տվյալների հավաքածուն՝ հասանելի https://www.slicer.org/wiki/Documentation/4.10/Training կայքում</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="22"/>
         <source>3D Slicer version 5</source>
         <extracomment>3_3DSlicerversion5_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D Slicer տարբերակ 5</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="24"/>
         <source>3D Slicer Scene</source>
         <extracomment>4_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D Slicer տեսարան</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="26"/>
@@ -72,31 +77,34 @@ Harvard Medical School</source>
 *The scene file and datasets have been saved as a MRB (Medical Reality Bundle) file. 
 *The MRB file format is Slicer&apos;s archive file format.</source>
         <extracomment>4_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">*Slicer տեսարանը MRML (Medical Reality Modeling Language) ֆայլ է, որը պարունակում է Slicer-ում բեռնված տարրերի ցուցակ (հատորներ, մոդելներ, ֆիդուցիալներ, փոխարկումներ և այլն)
+*Հաջորդ օրինակում մենք օգտագործում ենք «Slicer4minute.mrml» տեսարանը, որը բաղկացած է ՄՌՏ սկանից և գլխի 3D մոդելներից. 
+*Տեսարանի ֆայլը և տվյալների հավաքածուները պահպանվել են որպես MRB (Medical Reality Bundle) ֆայլ.  
+*MRB ֆայլի ֆորմատը Slicer-ի արխիվային ֆայլի ֆորմատն է։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="28"/>
         <source>Loading the Slicer4minute dataset</source>
         <extracomment>5_LoadingtheSlicer4minutedataset_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer4minute տվյալների հավաքածուի բեռնում</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="30"/>
         <source>Drag and drop the slicer4minute.mrb to load the scene in Slicer</source>
         <extracomment>5_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Սլայսերում տեսարանը բեռնելու համար քաշեք և գցեք slicer4minute.mrb ֆայլը։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="32"/>
         <source>Slicer4minute Scene</source>
         <extracomment>6_Slicer4minuteScene_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer4րոպե տեսարան</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="34"/>
         <source>Slicer displays the elements of the slicer4minute scene. The scene contains and MRI scan and 3D surface models of the brain.</source>
         <extracomment>6_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Slicer-ը ցուցադրում է slicer4minute տեսարանի տարրերը։ Տեսարանը պարունակում է ՄՌՏ սկան և ուղեղի 3D մակերեսային մոդելներ։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="36"/>
@@ -120,26 +128,27 @@ Harvard Medical School</source>
 14_3DVisualization_title
 ----------
 15_3DVisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D վիզուալիզացիա</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="38"/>
         <source>Select the module Models</source>
         <extracomment>7_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք «Մոդելներ» մոդուլը</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="40"/>
         <source>3D visualization</source>
         <extracomment>8_3Dvisualization_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3D վիզուալիզացիա</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="42"/>
         <source>Click on the pin icon on the top left corner of the red slice to display the slice Viewer menu.
 Click on the eye to display the axial slice in the 3D viewer</source>
         <extracomment>8_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Սեղմեք կարմիր շերտի վերին ձախ անկյունում գտնվող գամասեղի պատկերակին՝ շերտի դիտարկիչի մենյուի ցուցադրման համար։
+Սեղմեք աչքի պատկերակին՝ 3D դիտարկիչում առանցքային շերտը ցուցադրելու համար։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="46"/>
@@ -147,32 +156,35 @@ Click on the eye to display the axial slice in the 3D viewer</source>
 
 Slicer simultaneously displays the axial slice in the 3D viewer</source>
         <extracomment>9_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Օգտագործեք կարմիր դիտարկիչի սլայդերը՝ առանցքային ՄՌՏ շերտերը դիտելու համար։ 
+
+Slicer-ը միաժամանակ ցուցադրում է առանցքային շերտը 3D դիտարկիչում</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="50"/>
         <source>Select the Skin model and lower its opacity using the Opacity slider in the 3D Display tab</source>
         <extracomment>10_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք Skin մոդելը և 3D Display ներդիրի Opacity սլայդերով նվազեցրեք դրա թափանցիկությունը։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="52"/>
         <source>The skull_bone.vtk model appears through the skin.</source>
         <extracomment>10_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">skull_bone.vtk մոդելը երևում է մաշկի միջով։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="56"/>
         <source>Position the mouse in the 3D viewer, click on the left-mouse button to drag and rotate the model. 
 Click on the right-mouse button to zoom in and out</source>
         <extracomment>11_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Դիրքավորեք մկնիկը 3D դիտիչում, սեղմեք ձախ մկնիկի կոճակը՝ մոդելը քաշելու և պտտելու համար։ 
+Սեղմեք աջ մկնիկի կոճակը՝ մեծացնելու և փոքրացնելու համար</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="58"/>
         <source>Anatomical Views</source>
         <extracomment>12_AnatomicalViews_title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Անատոմիական տեսանկյուններ</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="60"/>
@@ -180,19 +192,21 @@ Click on the right-mouse button to zoom in and out</source>
 
 Click on the eye icon to display the axial and coronal slice in the 3D viewer</source>
         <extracomment>12_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Սեղմեք կարմիր և կանաչ դիտարկիչի վերին ձախ անկյունում գտնվող փինիկոնների վրա՝ շերտային դիտարկիչի մենյուի ցուցադրման համար
+
+Սեղմեք աչքի պատկերակին՝ 3D դիտարկիչում առանցքային և կորոնալ շերտերի ցուցադրման համար</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="64"/>
         <source>Turn off the visibility of the skull to display the brain white matter model</source>
         <extracomment>13_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Մարեք գանգի տեսանելիությունը՝ ուղեղի սպիտակ նյութի մոդելը ցուցադրելու համար։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="68"/>
         <source>The white matter surface, as well as the left and right optic nerves, appear in the viewer</source>
         <extracomment>14_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Դիտորդի մոտ երևում են սպիտակ նյութի մակերեսը, ինչպես նաև ձախ և աջ օպտիկ նյարդերը։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="72"/>
@@ -202,7 +216,11 @@ Check Clipping in the 3D Display tab
 
 In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; and check &apos;Negative&apos;</source>
         <extracomment>15_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ընտրեք hemispheric_white_matter.vtk մոդելը
+
+3D ցուցադրման ներդիրում նշեք «Կլիպինգը»
+
+Կլիպինգի հարթակների ներդիրում ընտրեք «Green Slice Clipping» տարբերակը և նշեք «Negative»-ը</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="76"/>
@@ -210,13 +228,15 @@ In the Clipping Planes tab, select the option &apos;Green Slice Clipping&apos; a
 
 *The Slicer5 training compendium contains a series of tutorials and pre-computed datasets to learn how to use the software.</source>
         <extracomment>16_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">*Այս ձեռնարկը կարճ ներածություն էր Slicer-ում ՄՌՏ տվյալների և 3D մոդելների ինտերակտիվ 3D վիզուալիզացիայի վերաբերյալ։
+
+*Slicer5-ի ուսուցողական ժողովածուն պարունակում է մի շարք ձեռնարկներ և նախահաշվարկված տվյալների հավաքածուներ՝ ծրագրաշարը օգտագործել սովորելու համար։</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="78"/>
         <source>Acknowledgements</source>
         <extracomment>17_TextBox_0</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Շնորհակալագրեր</translation>
     </message>
     <message>
         <location filename="text_dict_default.json" line="80"/>
@@ -228,7 +248,12 @@ Neuroimage Analysis Center
 NIH P41EB015902
 </source>
         <extracomment>17_TextBox_1</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Բժշկական պատկերների հաշվարկման ազգային դաշինք
+NIH U54EB005149
+
+Նյարդապատկերային վերլուծության կենտրոն
+NIH P41EB015902
+</translation>
     </message>
 </context>
 </TS>
