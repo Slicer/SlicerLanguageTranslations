0978abd7c897539c840625d50040070d635d557d
lassoan <307929+lassoan@users.noreply.github.com>
2026-09-08 16:03:00 +0000

    Automatic translations using DeepL

--- translations/SlicerRadiomics_de.ts
+++ translations/SlicerRadiomics_de.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="29"/>
         <source>Radiomics</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Radiomik</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="38"/>
@@ -15,194 +15,200 @@
     It gives access to the radiomics feature calculation classes implemented in pyradiomics library.
     See more details at http://pyradiomics.readthedocs.io/.
     </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">
+    Dies ist ein skriptbasiertes, ladbares Modul, das in der Erweiterung „SlicerRadomics“ enthalten ist.
+    Es ermöglicht den Zugriff auf die Klassen zur Berechnung von Radiomik-Merkmalen, die in der Bibliothek „pyradiomics“ implementiert sind.
+    Weitere Informationen finden Sie unter http://pyradiomics.readthedocs.io/.
+    </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="48"/>
         <source>
     This work was partially supported by NIH/NCI ITCR program grant U24 CA194354.
     </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">
+    Diese Arbeit wurde teilweise durch den Förderzuschuss U24 CA194354 des ITCR-Programms der NIH/NCI unterstützt.
+    </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="101"/>
         <source>Run the algorithm.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Führen Sie den Algorithmus aus.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="132"/>
         <source>Select Input Volume and Segmentation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">„Eingangsvolumen“ und „Segmentierung“ auswählen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="149"/>
         <source>Pick the input image for the feature calculation.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wählen Sie das Eingabebild für die Merkmalsberechnung aus.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="150"/>
         <source>Input Image Volume: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eingabebildvolumen: </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="165"/>
         <source>Pick the regions for feature calculation - defined by a segmentation or labelmap volume node.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wählen Sie die Regionen für die Merkmalsberechnung aus – definiert durch einen Segmentierungs- oder Labelmap-Volumenknoten.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="166"/>
         <source>Input regions: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eingabebereiche: </translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="171"/>
         <source>Extraction Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Anpassung der Extraktion</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="182"/>
         <source>Manual Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Manuelle Anpassung</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="187"/>
         <source>Parameter File Customization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Anpassung der Parameterdatei</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="203"/>
         <source>Feature Classes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Feature-Klassen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="209"/>
         <source>Features:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Merkmale:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="232"/>
         <source>Toggle Features:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Funktionen umschalten:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="234"/>
         <source>All Features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Alle Funktionen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="236"/>
         <source>Calculate all feature classes.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Alle Objektklassen berechnen.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="238"/>
         <source>No Features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Keine Funktionen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="240"/>
         <source>Calculate no feature classes.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Es werden keine Feature-Klassen berechnet.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="246"/>
         <source>Resampling and Filtering</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Resampling und Filterung</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="255"/>
         <source>Three floating-point numbers separated by comma defining the resampled pixel size (mm).</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Drei durch Kommas getrennte Gleitkommazahlen, die die Größe der neu abgetasteten Pixel (mm) angeben.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="255"/>
         <source>Resampled voxel size</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Neu berechnete Voxelgröße</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="261"/>
         <source>Laplacian of Gaussian filter kernel sizes (mm), separated by comma. If empty, no LoG filtering will be applied.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Kernelgrößen des Laplace-Gauß-Filters (mm), durch Kommas getrennt. Ist das Feld leer, wird keine LoG-Filterung angewendet.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="261"/>
         <source>LoG kernel sizes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">LoG-Kernelgrößen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="268"/>
         <source>If checked, PyRadiomics will calculate features on the image after applying wavelet transformation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wenn dieses Kontrollkästchen aktiviert ist, berechnet PyRadiomics nach der Anwendung der Wavelet-Transformation Merkmale für das Bild.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="268"/>
         <source>Wavelet-based features</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wavelet-basierte Merkmale</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="275"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Einstellungen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="289"/>
         <source>Set the bin width</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Breite des Behälters einstellen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="289"/>
         <source>Bin Width</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Behälterbreite</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="295"/>
         <source>Use a symmetrical GLCM matrix</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Verwenden Sie eine symmetrische GLCM-Matrix</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="295"/>
         <source>Enforce Symmetrical GLCM</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Symmetrisches GLCM erzwingen</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="310"/>
         <source>Parameter File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Parameterdatei</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="315"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ausgabe</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="324"/>
         <source>If checked, PyRadiomics outputs log messages from level DEBUG and higher (instead of INFO and higher)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wenn dieses Kontrollkästchen aktiviert ist, gibt PyRadiomics Protokollmeldungen ab der Stufe DEBUG aus (anstelle von INFO und höher).</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="324"/>
         <source>Verbose Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ausführliche Ausgabe</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="337"/>
         <source>Select the table where features will be saved, resets feature values on each run.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wählen Sie die Tabelle aus, in der die Merkmale gespeichert werden sollen; die Merkmalswerte werden bei jedem Durchlauf zurückgesetzt.</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="337"/>
         <source>Output table:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ausgabetabelle:</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="386"/>
         <source>Working...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wird bearbeitet...</translation>
     </message>
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="472"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Bewerben</translation>
     </message>
 </context>
 <context>
@@ -210,7 +216,7 @@
     <message>
         <location filename="SlicerRadiomics/SlicerRadiomics.py" line="30"/>
         <source>Informatics</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Informationstechnologie</translation>
     </message>
 </context>
 </TS>
